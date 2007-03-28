24ea60cd952af354b202095e251a048c60535171
andre.otero <andre.otero@7b3c32f5-e272-4d6f-9936-548b95b19007>
2007-03-28 19:27:47 +0000

    Alterado o FROM para scielo@bireme.org e o REPLY TO para o email do remetente.

--- common/SciELO/www/htdocs/applications/scielo-org/pages/services/sendMail.php
+++ common/SciELO/www/htdocs/applications/scielo-org/pages/services/sendMail.php
@@ -47,9 +47,42 @@ switch($acao){
 		$msg = str_replace("[articleURL]",$articleURL,$msg);
 		$msg = str_replace("[commentary]",$cgi["comment"],$msg);
 
+
+                $search = array ('@<script[^>]*?>.*?</script>@si', // Strip out javascript
+                                                 '@<[\/\!]*?[^<>]*?>@si',          // Strip out HTML tags
+                                                 '@([\r\n])[\s]+@',                // Strip out white space
+                                                 '@&(quot|#34);@i',                // Replace HTML entities
+                                                 '@&(amp|#38);@i',
+                                                 '@&(lt|#60);@i',
+                                                 '@&(gt|#62);@i',
+                                                 '@&(nbsp|#160);@i',
+                                                 '@&(iexcl|#161);@i',
+                                                 '@&(cent|#162);@i',
+                                                 '@&(pound|#163);@i',
+                                                 '@&(copy|#169);@i',
+                                                 '@&#(\d+);@e');                    // evaluate as php
+
+                $replace = array ('',
+                                                  '',
+                                                  '\1',
+                                                  '"',
+                                                  '&',
+                                                  '<',
+                                                  '>',
+                                                  ' ',
+                                                  chr(161),
+                                                  chr(162),
+                                                  chr(163),
+                                                  chr(169),
+                                                  'chr(\1)');
+
+                $msg_no_html = preg_replace($search, $replace, $msg);
+
+
 		$_mail = new PHPMailer();
-		$_mail->From     = $cgi["from"];
-		$_mail->FromName = $cgi["fromName"];
+		$_mail->AddReplyTo($cgi["from"],$cgi["fromName"]);
+		$_mail->From     = "appscielo@bireme.org";
+		$_mail->FromName = "Scielo";
 		$_mail->Subject  = $cgi["fromName"]." ".ARTICLE_SUGGESTION;
 		$_mail->Host     = "esmeralda.bireme.br";
 		$_mail->Password = "x@07sci@";
@@ -58,6 +91,7 @@ switch($acao){
 		$_mail->Mailer   = "smtp";
 		$_mail->IsHTML(true);
 		$_mail->Body = $msg;
+		$_mail->AltBody  = $msg_no_html;
 		$_mail->AddAddress($cgi["to"], $cgi['toName']);
 		$send = $_mail->Send();
 		if(!$send)
