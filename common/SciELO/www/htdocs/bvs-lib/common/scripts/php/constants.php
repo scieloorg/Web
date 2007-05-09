ef5e109d42bcbb18ede8b58a9c525401e963781c
fabio.batalha <fabio.batalha@7b3c32f5-e272-4d6f-9936-548b95b19007>
2007-05-09 21:04:29 +0000

    melhorias nos webservices das instâncias, removendo diretórios bvs-mod e bvs-lib que estavam no SciELO apenas para o funcionamento dos webservices. Devido as mudanças, agora não é mais necessário mante-los.

--- common/SciELO/www/htdocs/bvs-lib/common/scripts/php/constants.php
+++
@@ -1,4 +0,0 @@
-<?php
-	define("WXIS_PHP", "../../scripts/php/wxis.php");
-	define("NL", "\n<br>");
-?>
--- common/SciELO/www/htdocs/bvs-lib/common/scripts/php/xslt.php
+++
@@ -1,9 +0,0 @@
-<?
-    $DirName=dirname(__FILE__).'/';
-    $version = split ("\.", phpversion());
-	if ( $version[0] > 4 || ($version[0] == 4 && $version[1] >= 1) ) { 
-		include_once($DirName . "../../classes/php/xslt-4.1.php");    
-	} else {        
-		include_once($DirName . "../../classes/php/xslt-4.0.php");    
-	}
-?>
