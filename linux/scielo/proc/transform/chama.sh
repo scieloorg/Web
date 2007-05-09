a62a851cd694fd959d51eaa86d803d3b9c9bd3c3
fabio.batalha <fabio.batalha@7b3c32f5-e272-4d6f-9936-548b95b19007>
2007-05-09 21:23:56 +0000

    corrigindo estrutura de diretórios. mudando de /home/scielo/*  para /home/scielo/www/*

--- linux/scielo/proc/transform/chama.sh
+++
@@ -1,1 +0,0 @@
-java1_5_0/jre1.5.0_06/bin/java -server -Xincgc -ea -Djava.util.logging.config.file=transfLog.properties -Djavax.xml.transform.TransformerFactory=net.sf.saxon.TransformerFactoryImpl -cp .:XSLTransformer.jar:saxon8.jar XSLTransformer.XSLTServer 8899 xslts.ini 10
