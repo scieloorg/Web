fcf89b3afacd05e3cfaf5c5123e7f7481ae56ebf
deivid.martins <deivid.martins@7b3c32f5-e272-4d6f-9936-548b95b19007>
2007-06-14 20:42:53 +0000

    Criada a funcão buildGraphicByYear(stats, anoComeco, anoFinal)que constroi um gráfico para um determinado periodo de anos.

--- common/SciELO/www/htdocs/applications/scielo-org/classes/services/AccessServiceBar.php
+++ common/SciELO/www/htdocs/applications/scielo-org/classes/services/AccessServiceBar.php
@@ -161,6 +161,162 @@ para ter o gráfico "multi-lingüe"
 			$graph->Stroke();
 		}
 
+		/** Constroi o gráfico entre periodos de anos **/
+		function buildGraphicByYear($stats, $startYear, $lastYear)
+		{
+			$stat = $stats->getRequests();
+
+			// Pega todos os anos entre o Ano Inicial e o Ano Final 
+			$anos = array();
+			for($j = 0, $year = $startYear; $year<=$lastYear; $year++, $j++)
+			{
+				$anos[$j] = $year;
+			}
+
+			foreach ($stat as $s){
+				$mes = intval($s->getMonth());
+				$ano = intval($s->getYear());
+				$values[$ano][$mes] = $s->getNumberOfRequests();
+			}
+			
+			
+
+			/*
+			"gabarito" da linha dos graficos com 12 posicoes, uma para cada mes
+			os valores "-" sao considerados NULLs pelo jpgraph
+			*/
+			$data = array("0","0","0","0","0","0","0","0","0","0","0","0");
+			/*
+			"gabarito" para as cores das linhas do gráfico
+			*/
+			$cores = array("blue",
+								    "yellow",
+								    "purple",
+								    "cyan",
+								    "pink",
+								    "red",
+								    "orange",
+								    "green",
+								    "black",
+								    "sienna",
+									"darkred",
+									"darkgreen");
+		
+			// Create the graph. These two calls are always required
+//			$graph = new Graph(700,400,"auto");    
+			$graph = new Graph(900,300,"auto");    
+			$graph->SetScale("textlin");
+		
+		/*
+		cira um array bi-dimencional contendo array(ano{array com a quantidade de acessos por mes})
+		*/
+
+			$colorIndex = 0;
+			$bars = array();
+
+			foreach($values as $ano => $meses){
+		/*
+		valores será a variavel que servirá de "datasource" para a barra do grafico
+		inicialmente ele eh inicializada com o gabarito, e serao preenchidos os valores
+		dos meses no laço for logo abaixo
+		*/
+				$valores = $data;
+				
+				for($k = 0; $k <= count($valores); $k++){
+					if($meses[$k])
+						$valores[$k] = $meses[$k];
+				}
+
+				if(count($valores) > 12)
+				{
+					unset($valores[0]);
+				}
+
+				$valores = array_values($valores);
+
+				/*
+				aqui eu uso "Variáveis Variáveis" do PHP para poder
+				inserir vária linhas no gráfico
+				*/
+				$nome = "barplot".$ano;
+		
+				$$nome = new BarPlot($valores);
+
+				$cor = $cores[$colorIndex];
+
+				$$nome->SetFillColor($cor);
+				$$nome->SetColor($cor);		
+				/*configs para os valores do ponto*/
+				$$nome->value->SetColor("darkred"); 
+
+				$$nome->value->SetFont( FF_FONT1, FS_BOLD); 
+				$$nome->value->SetFormat( "%0.1d"); 
+//				$$nome->SetWeight(20);
+				
+				// Arrumando para um tamanho mais amigavel
+				if(count($anos)<3)
+				{
+					$$nome->SetWidth(20); 
+				}
+				else if(count($anos)< 4)
+				{
+					$$nome->SetWidth(15);
+				}
+				else if(count($anos)< 6)
+				{
+					$$nome->SetWidth(10); 
+				}
+				else if(count($anos)< 8)
+				{
+					$$nome->SetWidth(5); 
+				}
+				else if(count($anos)< 11)
+				{
+					$$nome->SetWidth(3); 
+				}
+
+				$$nome->value->Show();
+				$$nome->value->iHideZero = true;
+				$$nome->setLegend($ano);
+				/*adicionando a linha ao grafico*/
+ 				$colorIndex++;
+				
+				// Somente monta o gráfico dos anos exigidos pelo usuário
+				for($i = 0; $i<count($anos); $i++)
+				{
+					if($ano == $startYear+$i)
+					{
+						array_push($bars,$$nome);
+					}
+				}
+
+
+			}
+
+			$gbplot = new GroupBarPlot($bars);
+			$gbplot->SetWidth(0.9);
+			$graph->Add($gbplot);
+			$graph->yaxis->scale->SetGrace(20);
+			$graph->img->SetMargin(40,20,20,40);
+			$graph->title->Set(ARTICLE_ACCESS);
+			$graph->xaxis->title->Set(MONTHS);
+			$graph->yaxis->title->Set(ACCESSES);
+		
+			$graph->title->SetFont(FF_FONT1,FS_BOLD);
+			$graph->yaxis->title->SetFont(FF_FONT1,FS_BOLD);
+			$graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD);
+			$graph->SetShadow();
+
+			$graph->xaxis->SetTickLabels(explode(",",MONTH_LIST));
+
+			// Adjust the legend position
+//			$graph->legend->SetLayout(LEGEND_VER);
+			$graph->legend->Pos(.04,0.092,"","center");
+			$graph->legend->SetLayout(LEGEND_HOR); 
+			// Display the graph
+			$graph->Stroke();
+		}
+
 
 		function xxxbuildGraphic($stats){
 			$maior = 200;   // maior valor	
