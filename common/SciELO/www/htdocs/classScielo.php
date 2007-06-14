49f954c87530404a74f215d90932a86bf7695cc1
fabio.batalha <fabio.batalha@7b3c32f5-e272-4d6f-9936-548b95b19007>
2007-06-14 20:33:00 +0000

    Corrigindo commentário

--- common/SciELO/www/htdocs/classScielo.php
+++ common/SciELO/www/htdocs/classScielo.php
@@ -71,7 +71,7 @@ class Scielo extends ScieloBase
 //exibir o link Requests ?
 		$xmlScieloOrg .= "<requests>".$this->_def->getKeyValue("show_requests")."</requests>";
 
-//exibir o link Requests ?
+//exibir as Referencias do Artigo
 		$xmlScieloOrg .= "<show_article_references>".$this->_def->getKeyValue("show_article_references")."</show_article_references>";
 
 //path para o script de login
