b72ac20a62c008acffd7071fc76b2c9d0a5ee7cf
gustavo.fonseca <gustavo.fonseca@7b3c32f5-e272-4d6f-9936-548b95b19007>
2007-06-28 15:35:40 +0000

    Interface de relatórios de DOI

---
+++ common/scielo/www/htdocs/crossref/report.php
@@ -0,0 +1,49 @@
+<?php
+ini_set("display_errors","1");
+error_reporting(E_ALL ^E_NOTICE);
+//$lang = isset($_REQUEST['lang'])?($_REQUEST['lang']):"";
+
+
+require_once(dirname(__FILE__)."/../class.XSLTransformer.php");
+
+//require_once(dirname(__FILE__)."/../applications/scielo-org/users/langs.php");	
+
+require_once(dirname(__FILE__)."/../classDefFile.php");
+
+$defFile = parse_ini_file(dirname(__FILE__)."/../scielo.def");
+
+$applServer = $defFile["SERVER_SCIELO"];
+$databasePath = $defFile["PATH_DATABASE"];
+
+$serviceUrl = "http://" . $applServer . "/cgi-bin/wxis.exe/webservices/wxis/?IsisScript=search.xis&database=/home/scielo/www/bases/doi/crossref_DOIReport&search=ST=".$_REQUEST['stat'];
+$xmlFile = file_get_contents($serviceUrl);
+$xml = '<?xml version="1.0" encoding="ISO-8859-1"?>';
+$xml .='<root>';
+$xml .='<vars><from>'.$_REQUEST['from'].'</from><to>'.$_REQUEST['to'].'</to></vars>';
+$xml .= str_replace('<?xml version="1.0" encoding="ISO-8859-1"?>','',$xmlFile);
+$xml .='</root>';
+
+if($_REQUEST['getXML'] == "true"){
+				die($xml);
+			}
+?>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+		<!-- <link rel="stylesheet" href="/applications/scielo-org/css/public/style-<?=$lang?>.css" type="text/css" media="screen"/> -->
+	</head>
+	<body>
+		<?php
+			
+						
+			$xsl = dirname(__FILE__)."/report.xsl";
+			$transformer = new XSLTransformer();
+			$transformer->setXslBaseUri(dirname(__FILE__));
+			$transformer->setXML($xml);
+			$transformer->setXSL($xsl);
+			$transformer->transform();
+			$output = $transformer->getOutput();
+			echo (utf8_decode($output));
+			?>
+	</body>
+</html>
