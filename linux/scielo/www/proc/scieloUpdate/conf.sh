35f3f9327ac94a52bedf3727afc7ce3cf5483713
deivid.martins <deivid.martins@7b3c32f5-e272-4d6f-9936-548b95b19007>
2007-07-02 17:55:55 +0000

    Atualizador ScieloUpdate atualizado

---
+++ linux/scielo/www/proc/scieloUpdate/conf.sh
@@ -0,0 +1,12 @@
+# Arquivo de Configuração
+# login e senha do Subversion
+login=''
+senha=''
+# Endereço do Repositório
+svnLocal='svn://topazio.bireme.br/Bireme/scielo-metodologia/trunk'
+# Arquivos temporarios
+caminhoTemp='/home/scielo/repositorio'
+# Caminho onde será atualizado o scielo
+caminhoAPL='/home/scielo'
+reiniciaChama='false'
+CHAMA_HOME='/home/scielo/www/proc/transform'
