edb0deee1b44e0200b58f672a480fa9b480e97d5
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2007-07-18 20:39:05 +0000

    Função getSpecialXSL para retorna xsl _mathml ou _html no caso de sci_arttext e sci_abstract

---
+++ common/scielo/www/htdocs/classes/XMLFromIsisScript.php
@@ -0,0 +1,56 @@
+<?php
+
+class XMLFromIsisScript {
+	var $_xml;
+	function XMLFromIsisScript($xml){
+		$this->_xml = $xml;
+	}
+	function getXml(){
+		$this->_xml = $this->fixUtfEntities($this->_xml); // 200603
+		$this->_xml= $this->replaceThisByContent($this->_xml); // 200603
+		return $this->_xml;
+	}
+	function getSpecialXSL(){
+		if (strpos($this->_xml, '<mml:math ')>0){
+			$xsl = "_mathml";
+		} else {
+			$xsl = "_html";
+		}
+		return $xsl;
+	}
+	function fixUtfEntities($s){
+		$s = str_replace('&#x2028;', '<p/>', $s);
+		$s = str_replace('&#710;', '^', $s);
+		
+		return $s;
+	}
+
+	function replaceThisByContent($parXML){
+		$xml = $parXML;
+		
+		$p = strpos($xml, '[replace-this-by-content]');
+		if ($p >0) {
+			$pf = strpos($xml, '[/replace-this-by-content]');
+			$expression = substr($xml,$p, $pf-$p).'[/replace-this-by-content]';
+			$filename = str_replace('[replace-this-by-content]', '', str_replace('[/replace-this-by-content]', '', $expression));
+			$body = $this->read($filename);
+
+			$bodyBegin = strpos($body, '<body');
+			$body = substr($body, $bodyBegin);
+			$bodyBegin = strpos($body, '>');
+			$body = substr($body, $bodyBegin+1);
+			$bodyEnd = strpos($body, '</body>');
+			$body = substr($body, 0, $bodyEnd);
+
+			$xml = str_replace($expression, '<!-- inicio arquivo -->'.$body.'<!-- fim arquivo -->', $xml);
+			$xml = $this->replaceThisByContent($xml);
+		} 
+		return $xml;
+	}
+	function read($filename){
+		$f = fopen($filename, "r");
+		$s = fread($f, 100*1024);
+		return $s;
+	}
+}
+?>
