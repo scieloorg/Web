4f357e7f79c4193e690c93703f0c494014da5ccd
gustavo.fonseca <gustavo.fonseca@7b3c32f5-e272-4d6f-9936-548b95b19007>
2007-08-03 12:52:44 +0000

    SMT-66

--- common/scielo/www/htdocs/class.XSLTransformSocket.php
+++ common/scielo/www/htdocs/class.XSLTransformSocket.php
@@ -1,13 +1,18 @@
 <?php
+include_once (dirname(__FILE__)."/classes/class_xml_check/class_xml_check.php"); //classe para verificacao de XML
+include_once (dirname(__FILE__)."/classes/phpmailer/class.phpmailer.php"); //classe para envio de email
+include_once (dirname(__FILE__)."/class.XSLTransformer.php");
+
 class XSLTransformerSocket {
-	var $xsl, $xml, $host, $port, $FINISH, $END_OF_MESS_SYMBOL, $socket, $output;
+	var $xsl, $xml, $host, $port, $FINISH, $END_OF_MESS_SYMBOL, $socket, $output, $defFile;
 	/* Constructor  */	 
 	function XSLTransformerSocket($host,$port) { 
+		$this->defFile = parse_ini_file(dirname(__FILE__)."/scielo.def",true);
 		$this->host = $host;
 		$this->port = $port;
 		$this->END_OF_MESS_SYMBOL = "?<==>?";		
 		$this->FINISH = "?<++>?";
-		$this->socket = fsockopen($host,$port, $errno, $errstr);
+		$this->socket = @fsockopen($host,$port, $errno, $errstr);
 		if ($this->socket)
 		{
 			putenv("ENV_SOCKET=true");
@@ -29,18 +34,14 @@ class XSLTransformerSocket {
 	}
 
 	function transform($xsl, $xml){
-	  if (!$this->socket)
+	 if (!$this->socket)
 	  {
 	   	die("$errstr ($errno)<br />\n");
 	  }
 		$aspas = array(chr(147),chr(148));
 		$menos = array(chr(150));
-		
-		if (strpos(strtolower($xml),'utf-')>0) {
-			$xml = utf8_decode($xml);
-		}else {
-			$iso = true;
-        }
+
+		$xml = utf8_decode($xml);
 		$xml = str_replace($aspas,"&quot;",$xml);
 		$xml = str_replace($menos,"-",$xml);
 		$xml = str_replace("\n","",$xml);
@@ -49,21 +50,33 @@ class XSLTransformerSocket {
 		$xml = str_replace(chr(134),"",$xml);
 		$xml = str_replace(chr(145),"",$xml);
 		$xml = str_replace(chr(146),"",$xml);
-	  //fwrite($this->socket, "ISO-8859-1:".$xsl.":".$xml."\n") or die("1");
+		//fwrite($this->socket, "ISO-8859-1:".$xsl.":".$xml."\n") or die("1")
+	
+		if($this->defFile['XML_ERROR']['ENABLED_XML_ERROR'] == '1'){
+			$xmlCheck = new XML_check();
+			$xml1 = $xml;
+			if($xmlCheck->check_string($xml1)){ // verifica se o XML é bem formado
+				fwrite($this->socket, "utf-8:".$xsl.":".$xml."\n") or die("1");
+				fwrite($this->socket, $this->END_OF_MESS_SYMBOL."\n") or die("2");
+				$message = ($this->recebeResultado() . "<!-- XML well formed verifier ON -->");
+			}else{
+				include_once (dirname(__FILE__)."/mail_msg.php");
+				$url = "http://".$_SERVER['SERVER_NAME']."/php/xmlError.php?lang=".$_REQUEST['lng'];
+				exit('<META http-equiv="refresh" content="0;URL='.$url.'">');
+			}
+		}else{
+			fwrite($this->socket, "utf-8:".$xsl.":".$xml."\n") or die("1");
+			fwrite($this->socket, $this->END_OF_MESS_SYMBOL."\n") or die("2");
+			$message = ($this->recebeResultado() . "<!-- XML well formed verifier OFF -->");
+		}
 
-	  fwrite($this->socket, "utf-8:".$xsl.":".$xml."\n") or die("1");
+		//var_dump($xml);
+		//var_dump($xsl);
+		//var_dump($message);
+		//$message =  utf8_decode($message);
+		//die($message);
 
-	  fwrite($this->socket, $this->END_OF_MESS_SYMBOL."\n") or die("2");
-	  $message = $this->recebeResultado();
-      if ($iso) $message = utf8_decode($message);
-//var_dump($xml);
-//var_dump($xsl);
-//var_dump($message);
-	  //$message =  utf8_decode($message);
-//die($message);
-//
-
-	  return $message;
+	 	return $message;
 	}
 
 
