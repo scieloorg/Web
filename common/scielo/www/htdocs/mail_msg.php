f670090a8f4770f007df4911961cf1d0ae5392f1
gustavo.fonseca <gustavo.fonseca@7b3c32f5-e272-4d6f-9936-548b95b19007>
2007-09-10 16:33:10 +0000

    Implementar log no servidor para a saída dos erros de XML mal formado. (SMT-66)

--- common/scielo/www/htdocs/mail_msg.php
+++ common/scielo/www/htdocs/mail_msg.php
@@ -9,38 +9,80 @@ $msg_no_html .= "Server IP: " . $_SERVER['SERVER_ADDR'] . "\n";
 $msg_no_html .= "URL: " .$_SERVER['REQUEST_URI'] . "\n\n";
 $msg_no_html .= "XML: \n" . $xml . "\n";
 
-//Formato: Ano-Mes-Dia-Hora_Minuto_Segundo
-$fileName = "XMLerror_".date("y-m-d-H_i_s").".xml";
+if($defFile['ENABLED_MAIL_ALERT'] == '1'){
+	//Formato: Ano-Mes-Dia-Hora_Minuto_Segundo
+	$fileName = "XMLerror_".date("y-m-d-H_i_s").".xml";
 
+	$_mail = new PHPMailer();
+	//$_mail->AddReplyTo($cgi["from"],$cgi["fromName"]);
+	$_mail->From     = "scielo@bireme.org";
+	$_mail->FromName = "Scielo";
+	$_mail->Subject  = "SciELO Error Report";
+	$_mail->Host     = "esmeralda.bireme.br";
+	$_mail->Password = "x@07sci@";
+	$_mail->Username = "appscielo";
+	$_mail->SMTPAuth =true;
+	$_mail->Mailer   = "smtp";
+	$_mail->IsHTML(false);
+	//$_mail->Body = $msg;
+	$_mail->AltBody  = $msg_no_html;
+	$_mail->AddAddress($defFile['MAILTO_XML_ERROR'],$defFile['NAMETO_XML_ERROR']);
+	$_mail->AddStringAttachment($xml,$fileName,"base64","text/xml");
+	$send = $_mail->Send();
 
-$_mail = new PHPMailer();
-//$_mail->AddReplyTo($cgi["from"],$cgi["fromName"]);
-$_mail->From     = "scielo@bireme.org";
-$_mail->FromName = "Scielo";
-$_mail->Subject  = "SciELO Error Report";
-$_mail->Host     = "esmeralda.bireme.br";
-$_mail->Password = "x@07sci@";
-$_mail->Username = "appscielo";
-$_mail->SMTPAuth =true;
-$_mail->Mailer   = "smtp";
-$_mail->IsHTML(false);
-//$_mail->Body = $msg;
-$_mail->AltBody  = $msg_no_html;
-$_mail->AddAddress($defFile['MAILTO_XML_ERROR'],$defFile['NAMETO_XML_ERROR']);
-$_mail->AddStringAttachment($xml,$fileName,"base64","text/xml");
-$send = $_mail->Send();
+	if(!$send){
+		if(!$fileHandle = fopen($fileName,'w')){
+			print "Erro abrindo o arquivo $fileName";
+			exit;
+		}
+		if(!fwrite($fileHandle,$xml)){
+				print "Erro escrevendo no arquivo $fileName";
+		}else{
+				print "Arquivo não pode ser escrito.";
+		}
 
-if(!$send){
-	if(!$fileHandle = fopen($fileName,'w')){
-		print "Erro abrindo o arquivo $fileName";
-		exit;
 	}
-	if(!fwrite($fileHandle,$xml)){
-	        print "Erro escrevendo no arquivo $fileName";
+}
+if($defFile['ENABLED_LOG_XML_ERROR'] == '1'){
+	$logName = $defFile['LOG_XML_ERROR_FILENAME'];
+
+	$logContent = "########################################### \n";
+	$logContent .= $msg_no_html;
+	$logContent .= "########################################### \n";
+
+	if(is_writable($logName)){
+		//Abrindo o arquivo configurado em $logName em modo Append(Acrescimo).
+		if(!$handle = fopen($logName,'a')){
+			print "Erro abrindo o arquivo $logName .";
+			exit;
+		}
+		//Escrevendo no arquivo configurado em $logName.
+		if(!fwrite($handle,$logContent)){
+			print "Erro escrevendo no arquivo $logName .";
+			exit;
+		}
+		//Finalizando o handler $handle.
+		fclose($handle);
 	}else{
-	        print "Arquivo não pode ser escrito.";
-	}
+		$fileName = "XMLerror_".date("y-m-d-H_i_s").".xml";
 
-}												
-	
+		$_mail = new PHPMailer();
+		//$_mail->AddReplyTo($cgi["from"],$cgi["fromName"]);
+		$_mail->From     = "scielo@bireme.org";
+		$_mail->FromName = "Scielo";
+		$_mail->Subject  = "SciELO Error Report";
+		$_mail->Host     = "esmeralda.bireme.br";
+		$_mail->Password = "x@07sci@";
+		$_mail->Username = "appscielo";
+		$_mail->SMTPAuth =true;
+		$_mail->Mailer   = "smtp";
+		$_mail->IsHTML(false);
+		//$_mail->Body = $msg;
+		$_mail->AltBody  = $msg_no_html;
+		$_mail->AddAddress($defFile['MAILTO_XML_ERROR'],$defFile['NAMETO_XML_ERROR']);
+		$_mail->AddStringAttachment($xml,$fileName,"base64","text/xml");
+		$send = $_mail->Send();
+		}
+
+}
 ?>
