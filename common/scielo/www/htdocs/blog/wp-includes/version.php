82afebb98bbd29ce6e902f1da23775c526bb2330
jamil.atta <jamil.atta@7b3c32f5-e272-4d6f-9936-548b95b19007>
2007-12-13 16:42:16 +0000

    Removed file/folder

--- common/scielo/www/htdocs/blog/wp-includes/version.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// This holds the version number in a separate file so we can bump it without cluttering the SVN
-
-$wp_version = '2.3.1';
-$wpmu_version = '1.3';
-$wp_db_version = 6124;
-
-?>
