c9b8379edadb29a0f8704d21d62b3c5f1b36fa23
fabio.batalha <fabio.batalha@7b3c32f5-e272-4d6f-9936-548b95b19007>
2007-12-19 22:22:11 +0000

    Mudanças para impedir que os robos que acessam scielo sejam redirecionados para o checkLogin do SciELO ORG. Os robos entravam em loop ao acessar a home da scielo utilizando wget.

--- common/scielo/www/htdocs/applications/scielo-org/sso/header.php
+++ common/scielo/www/htdocs/applications/scielo-org/sso/header.php
@@ -5,47 +5,61 @@
 session_start();
 $dir = dirname(__FILE__);
 $defi = parse_ini_file($dir."/../../../scielo.def",true);
-if($defi['services']['show_login'] != "0"){
+$robotsUserAgents = parse_ini_file($dir."/../../../robotsUserAgents.def",true); 
 
-$loginURL = "http://".$defi['SCIELO_REGIONAL']['SCIELO_REGIONAL_DOMAIN']. $defi['SCIELO_REGIONAL']['check_login_url'];
-	if(isset($_GET['userID']))
-	{
-			if (strpos($_SERVER["REQUEST_URI"],"lng"))
-			{
-				$self_url = str_replace("lng=en","lng=".$_GET['lng'],"http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]);
-			}else{
-				$self_url = "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]."&lng=".$_GET['lng'];
-			}
-			$inicio = strpos($self_url,"userID") -1 ;
-			$self_url = substr($self_url, 0, $inicio);
+$isaRobot = false;
 
-			header('P3P: CP="NOI ADM DEV PSAi COM NAV OUR OTRo STP IND DEM"');
-			setcookie("userID",$_GET['userID'],time()+3600,"/");
-			setcookie("userToken",$_GET['userToken'],time()+3600,"/");
-			setcookie("tokenVisit",$_GET['tokenVisit'],time()+3600,"/");
-			setcookie("firstName",$_GET['firstName'],time()+3600,"/");
-			setcookie("lastName",$_GET['lastName'],time()+3600,"/");
-			setcookie("userToken",$_GET['userToken'],time()+3600,"/");
-			setcookie("email",$_GET['email'],time()+3600,"/");
-			session_write_close();
-			Header("Location: ".$self_url);
-			exit;
+foreach($robotsUserAgents["ROBOTS_AGENT"] as $key => $value){
+	$userAgent = strtolower($HTTP_USER_AGENT);
+	$agent = strtolower($value);
+	if (strstr($userAgent,$agent)){
+		$isaRobot = true;
+		break;
 	}
-	/*
-	se nao verificou no Regional o Login do usuario vai verificar
-	*/
-	if(!isset($_SESSION['checkedLogin']))
-	{
-		$self_url = "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-		$inicio = strpos($self_url,"userID") -1 ;
+}
+if (!$isaRobot){
+	if($defi['services']['show_login'] != "0"){
 
-		if($inicio > 0){
-		   $self_url = substr($self_url, 0, $inicio);
+	$loginURL = "http://".$defi['SCIELO_REGIONAL']['SCIELO_REGIONAL_DOMAIN']. $defi['SCIELO_REGIONAL']['check_login_url'];
+		if(isset($_GET['userID']))
+		{
+				if (strpos($_SERVER["REQUEST_URI"],"lng"))
+				{
+					$self_url = str_replace("lng=en","lng=".$_GET['lng'],"http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]);
+				}else{
+					$self_url = "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]."&lng=".$_GET['lng'];
+				}
+				$inicio = strpos($self_url,"userID") -1 ;
+				$self_url = substr($self_url, 0, $inicio);
+	
+				header('P3P: CP="NOI ADM DEV PSAi COM NAV OUR OTRo STP IND DEM"');
+				setcookie("userID",$_GET['userID'],time()+3600,"/");
+				setcookie("userToken",$_GET['userToken'],time()+3600,"/");
+				setcookie("tokenVisit",$_GET['tokenVisit'],time()+3600,"/");
+				setcookie("firstName",$_GET['firstName'],time()+3600,"/");
+				setcookie("lastName",$_GET['lastName'],time()+3600,"/");
+				setcookie("userToken",$_GET['userToken'],time()+3600,"/");
+				setcookie("email",$_GET['email'],time()+3600,"/");
+				session_write_close();
+				Header("Location: ".$self_url);
+				exit;
 		}
-		$_SESSION['checkedLogin'] = "true";
-		session_write_close();
-		$self_url = '?origem='.str_replace('?','&',$self_url);
-		header("Location: ".$loginURL.$self_url);
+		/*
+		se nao verificou no Regional o Login do usuario vai verificar
+		*/
+		if(!isset($_SESSION['checkedLogin']))
+		{
+			$self_url = "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+			$inicio = strpos($self_url,"userID") -1 ;
+	
+			if($inicio > 0){
+			   $self_url = substr($self_url, 0, $inicio);
+			}
+			$_SESSION['checkedLogin'] = "true";
+			session_write_close();
+			$self_url = '?origem='.str_replace('?','&',$self_url);
+			header("Location: ".$loginURL.$self_url);
+		}
 	}
 }
 ?>
