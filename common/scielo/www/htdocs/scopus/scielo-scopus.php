f083f398fd8c5d8c7cacfec87b7a0b24a6533642
jamil.atta <jamil.atta@7b3c32f5-e272-4d6f-9936-548b95b19007>
2008-02-27 19:07:44 +0000

    Alteração referente a testes junto com Scopus correspondente a tarefa smt-151 do jira.

--- common/scielo/www/htdocs/scopus/scielo-scopus.php
+++ common/scielo/www/htdocs/scopus/scielo-scopus.php
@@ -1,6 +1,6 @@
 <?php 
 	/*
-	 * Parâmetro do Header
+	 * Parï¿½metro do Header
 	 */
 	$ReqId = $_REQUEST['reqid'];
 	$Ver = $_REQUEST['ver'];
@@ -8,13 +8,13 @@
 	$OpaqueInfo = $_REQUEST['opaqueinfo'];
 	$LogLevel = $_REQUEST['loglevel'];
 	/*
-	 * Parâmetro do Body
+	 * Parï¿½metro do Body
 	 */
 	$absMetSource = $_REQUEST['absmetsource'];
 	$responseStyle = $_REQUEST['responsestyle'];
 	$dataReponseStyle = $_REQUEST['dataresponsestyle'];
 	/*
-	 * Parâmetro para requisição
+	 * Parï¿½metro para requisiï¿½ï¿½o
 	 */
 	$PID = $_REQUEST['issn'].$_REQUEST['ano'].$_REQUEST['volume'].$_REQUEST['number'];
 	$PID = 'S'.$PID.'$'; 
@@ -22,37 +22,33 @@
 	$posUrl = strpos($Server,'/');
 	$Server = substr($Server, 0, $posUrl); 
 	/*
-	 * Parâmetro para header
+	 * Parï¿½metro para header
 	 */
 	$header = '<?xml version="1.0" encoding="ISO-8859-1"?>
 				<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">';
-	$header .= '<soapenv:Header>';
 	$header .= '<EASIReq xmlns="http://webservices.elsevier.com/schemas/easi/headers/types/v1">';
 	$header .= '<ReqId xmlns="">'.$ReqId.'</ReqId>';
 	$header .= '<Ver xmlns="">'.$Ver.'</Ver>';
 	$header .= '<Consumer xmlns="">'.$Consumer.'</Consumer>';
-	$header .= '<ConsumerClient xmlns="">tester_client</ConsumerClient>';
+	$header .= '<ConsumerClient xmlns="">SCIELO</ConsumerClient>';
 	$header .= '<OpaqueInfo xmlns="">'.$OpaqueInfo.'</OpaqueInfo>';
 	$header .= '<LogLevel xmlns="">'.$LogLevel.'</LogLevel>';
 	$header .= '</EASIReq>';
-	$header .= '</soapenv:Header>';
-	$header .= '<soapenv:Body>';
 	$header .= '<getCitedByCount xmlns="http://webservices.elsevier.com/schemas/metadata/abstracts/types/v7">';
 	$header .= '<getCitedByCountReqPayload>';
 	$header .= '<dataResponseStyle>'.$dataReponseStyle.'</dataResponseStyle>';
 	$header .= '<absMetSource>'.$absMetSource.'</absMetSource>';
 	$header .= '<responseStyle>'.$responseStyle.'</responseStyle>';
 	/*
-	 * Parâmetro para body
+	 * Parï¿½metro para body
 	 */
 	$content= file_get_contents("http://" . $Server . "/cgi-bin/wxis.exe/?IsisScript=ScieloXML/sci_scopus_cited_by_count.xis&database=artigo&search=HR=".$PID);
 	$contentrp = str_replace('<?xml version="1.0" encoding="ISO-8859-1"?>','',$content);
 	/*
-	 * Parâmetro para footer
+	 * Parï¿½metro para footer
 	 */
 	$footer = '</getCitedByCountReqPayload>';
 	$footer .= '</getCitedByCount>';
-	$footer .= '</soapenv:Body>';
 	$footer .= '</soapenv:Envelope>';
 	
 	header('Content-type: application/xml');
