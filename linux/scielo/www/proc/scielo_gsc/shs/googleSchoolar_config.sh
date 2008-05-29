4f1fa4f7eaf48492fc4f0f6ac2ac6bc346a7050c
fabio.batalha <fabio.batalha@7b3c32f5-e272-4d6f-9936-548b95b19007>
2008-05-29 16:46:06 +0000

    configurando path para caminho padrão de instalação do SciELO.

--- linux/scielo/www/proc/scielo_gsc/shs/googleSchoolar_config.sh
+++ linux/scielo/www/proc/scielo_gsc/shs/googleSchoolar_config.sh
@@ -1,6 +1,6 @@
 ##################
 # variáveis com caminho para bases de dados utilizadas no processmento.
 ##################
-export database_title="/srv/scielo/www/bases/title/title"
-export database_article="/srv/scielo/www/bases/artigo/artigo"
-export database_issue="/srv/scielo/www/bases/issue/issue"
+export database_title="/www/scielo/www/bases/title/title"
+export database_article="/www/scielo/www/bases/artigo/artigo"
+export database_issue="/www/scielo/www/bases/issue/issue"
