1aa8431398c2a891964816455373d99de661a098
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2008-10-01 17:58:09 +0000

    Processamento de idiomas

--- linux/scielo/www/proc/proc_langs/linux/config/getConfig.bat
+++ linux/scielo/www/proc/proc_langs/linux/config/getConfig.bat
@@ -1,2 +1,2 @@
 export my_mx=$1
-$my_mx lw=9999 seq=proc_langs/text-langs.def "pft=if p(v1) or size(v1)>0 then 'export ',v1 fi,#" now > proc_langs/config.bat
+$my_mx seq=proc_langs/text-langs.def lw=9999 "pft=if p(v1) or size(v1)>0 then 'export ',v1 fi,#" now > proc_langs/config.bat
