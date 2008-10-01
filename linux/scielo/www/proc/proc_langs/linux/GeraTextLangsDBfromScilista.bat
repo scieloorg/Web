92b11b54eef5efad689b5527e3642755f283f959
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2008-10-01 19:39:33 +0000

    Processamento de idiomas

--- linux/scielo/www/proc/proc_langs/linux/GeraTextLangsDBfromScilista.bat
+++ linux/scielo/www/proc/proc_langs/linux/GeraTextLangsDBfromScilista.bat
@@ -6,7 +6,7 @@ else
 	proc_langs/linux/config/getConfig.bat $2
 	proc_langs/config.bat
 
-	$MX "seq=$1 " lw=9000 "pft=if p(v1) then 'call proc_langs/linux/GeraTextLangsDB.bat ',v1,x1,v2,x1,'$2'/ fi" now >temp/GeraTextLangsDB.bat
+	$MX "seq=$1 " lw=9000 "pft=if p(v1) then 'call proc_langs/linux/GeraTextLangsDB.bat ',v1,x1,v2,x1,'$3'/ fi" now >temp/GeraTextLangsDB.bat
 
 	chmod +x temp/GeraTextLangsDB.bat
 	temp/GeraTextLangsDB.bat
--- windows/scielo/www/proc/proc_langs/win/GeraTextLangsDBfromScilista.bat
+++ windows/scielo/www/proc/proc_langs/win/GeraTextLangsDBfromScilista.bat
@@ -5,7 +5,7 @@ set PATH=%PATH%;.
 call proc_langs\win\config\getConfig.bat %2
 call proc_langs\config.bat
 
-cisis\mx "seq=%1 " lw=9000 "pft=if p(v1) then 'call proc_langs\win\GeraTextLangsDB.bat ',v1,x1,v2,x1,'%2'/ fi" now >temp\GeraTextLangsDB.bat
+cisis\mx "seq=%1 " lw=9000 "pft=if p(v1) then 'call proc_langs\win\GeraTextLangsDB.bat ',v1,x1,v2,x1,'%3'/ fi" now >temp\GeraTextLangsDB.bat
 call temp\GeraTextLangsDB.bat
 
 goto END
