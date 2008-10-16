2cc31cc587a46a61b30c6e74e12de6911a4bf2e6
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2008-10-16 21:00:51 +0000

    Processamento de idiomas ( SMT-315)

--- linux/scielo/www/proc/proc_langs/linux/GeraScilistaCompleta.bat
+++ linux/scielo/www/proc/proc_langs/linux/GeraScilistaCompleta.bat
@@ -3,6 +3,5 @@ if [ "@$2" == "@" ] then
 	echo Missing the second parameter: mx 	
 else
 	proc_langs/linux/config/getConfig.bat $2
-	proc_langs/config.bat
 	$MX $TITLE lw=9000 "pft=if v50='C' then v68,x1,'$'/ fi" now > $1
 fi
