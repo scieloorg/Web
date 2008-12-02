f39ad92311c289fc8ef64c47e7b6c0339ef78ecf
gustavo.fonseca <gustavo.fonseca@7b3c32f5-e272-4d6f-9936-548b95b19007>
2008-12-02 17:50:08 +0000

    Logs de acesso aos serviços do artigo em base Isis

--- common/scielo/www/htdocs/applications/scielo-org/js/httpAjaxHandler.js
+++ common/scielo/www/htdocs/applications/scielo-org/js/httpAjaxHandler.js
@@ -2,6 +2,7 @@ var xmlHttp = false;
 var metaengine = "";
 var rateResult = "";
 
+
 function httpInit()
 {
 	if (window.XMLHttpRequest)
@@ -10,9 +11,9 @@ function httpInit()
 
 		if (xmlHttp.overrideMimeType)
 		{
-            	xmlHttp.overrideMimeType('text/xml');
+            	xmlHttp.overrideMimeType('text/plain');
         }
-     	}else if (window.ActiveXObject)
+     }else if (window.ActiveXObject)
 		{ // IE
 			try {
            		xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
@@ -21,7 +22,9 @@ function httpInit()
         		try {
         			xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
         		}catch (e) {
+					alert('fodeo1');
 				}
+				alert('fodeo2');
         	}
      	}
 		if (!xmlHttp) {
@@ -39,92 +42,31 @@ function httpInit()
  * @param serviceName String nome do serviço acessado
  *
  */
-function callUpdateArticleLog( serviceName )
+function callUpdateArticleLog( serviceName ) /* modified by Gustavo Fonseca (gustavo.fonseca@bireme.org) */
 {
-	// Informações sobre o visitante
-	var browserV = navigator.appName + " ..#.. " + navigator.appCodeName +
-		" ..#.. " + navigator.appVersion + " ..#.. " + navigator.userAgent;
-	var idiomaV;
-	var resolucaoV = screen.width +"X"+ screen.height+" "+ screen.colorDepth +" bits";
-	var SOV = navigator.platform;	// Sistema Operacional
-	var suporteV = "";				// Mime-types suportados pelo cliente
-	var tituloV = document.title;	// Titulo do Documento
-	var urlV = document.URL;		// URL do Documento
+	var urlV = document.URL; /* URL do Documento */
+	var pid = urlV.substr(urlV.lastIndexOf('pid=')+4,23);
+	var lang = urlV.substr(urlV.lastIndexOf('tlng=')+5,2);
 
-	var dados = "";					// Dados a serem enviados
-	var url = "/applications/scielo-org/ajax/updateLog.php";	//	url para onde mandamos os dados
-	var i = 0;
-
-	// O IE trata de um jeito a linguagem padrão, e o Firefox e Opera de outra maneira
-	if(!navigator.systemLanguage)
-		idiomaV = navigator.language;
-	else
-		idiomaV = navigator.systemLanguage;
+	var dados = "";	/* Dados a serem enviados */
+	var url = "/applications/scielo-org/ajax/updateIsisLog.php"; /* url para onde mandamos os dados */
 
 	serviceName = serviceName.toString();
 
-	// Personalizando nome dos serviços
-	if(serviceName.replace(/ /g,"") == "access")
-		serviceName = "estatisticas_de_acesso";
-	else if(serviceName.replace(/ /g,"") == "cited_Google")
-		serviceName = "citado_por_google";
-	else if(serviceName.replace(/ /g,"") == "related")
-		serviceName = "similares_em_scielo";
-	else if(serviceName.replace(/ /g,"") == "related_Google")
-		serviceName = "similares_em_google";
-	else if(serviceName.replace(/ /g,"") == "send_mail")
-		serviceName = "enviar_este_artigo_por_email";
-	else if(serviceName.replace(/ /g,"") == "cited_SciELO"){
-		serviceName = "citado_por_scielo";
-	}
+	dados = (String)("?serviceName=" + serviceName + "&pid=" + pid + "&lang=" + lang);
 
-
-	if(navigator.appName != "Microsoft Internet Explorer")
-	{
-		// Agora sabemos por exemplo se o visitante suporta Flash, Applet, etc...
-		for(i = 0; i < navigator.mimeTypes.length - 1; i++)
-			suporteV += navigator.mimeTypes[i].type + " ..#.. ";
-		suporteV += navigator.mimeTypes[i].type;
-	}
-	// OBS: Implementar na proxima versão detector de mimetypes para o IE
-
-	dados = (String)("servico=" + serviceName + "&browser=" + browserV.replace(/;/g, " ")
-		+ "&idioma=" + idiomaV.replace(/;/g, " ") + "&resolucao=" + resolucaoV.replace(/;/g, " ")
-		+ "&SO=" + SOV.replace(/;/g, " ") + "&suporte=" + suporteV.replace(/;/g, " ")
-		+ "&url=" + escape(urlV.replace(/;/g, " ")) + "&titulo=" + escape(tituloV.replace(/;/g, " ")) );
-
-	// inicializa a XMLHttpRequest
+	/* inicializa a XMLHttpRequest */
 	httpInit();
-
-	xmlHttp.open("POST", url, true);
+	xmlHttp.open("GET", url+dados, true);
 	xmlHttp.onreadystatechange = UpdateRate;
 
-	// Definimos os Headers para usarmos o método POST
-	xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	xmlHttp.setRequestHeader("Content-Length",dados.length);
-	xmlHttp.setRequestHeader("Cache-Control","no-store, no-cache, must-revalidate");
-	xmlHttp.setRequestHeader("Cache-Control","post-check=0, pre-check=0");
-	xmlHttp.setRequestHeader("Pragma", "no-cache");
-
-	// Enviamos os dados via POST
-    xmlHttp.send(dados);
+    xmlHttp.send(null);
 }
 
 function UpdateRate(){
-    //var resultPortlet = document.getElementById("searchResult");
-    //var result = document.getElementById("result");
-    //var buffer = "";
-    //resultPortlet.style.display="block";
-
-    //result.innerHTML = "<div align='center'><img src='../image/common/loading.gif' border='0'></div>";
-
     if (xmlHttp.readyState == 4) {
-
         if (xmlHttp.status == 200) {
-      	    //var responseXml = xmlHttp.responseXML;
-   			var responseText = xmlHttp.responseText;
-			rateResult = responseText;  //descomentado
-
+   			ajaxResponse = xmlHttp.responseText;
         }
     }
 }
