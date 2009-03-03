4901e249907a2bc2252ac431eb8a85a0ebcc2bab
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-03-03 18:42:36 +0000

    processamento de idiomas - substituicao de IsisScript por shell scripts

--- proc/procLangs/linux/deleteRecordsDBLang.bat
+++ proc/procLangs/linux/deleteRecordsDBLang.bat
@@ -9,8 +9,3 @@ then
 fi
 
 $MX $DBLANG  btell=0  "IS=$ACRON$ISSUEID" "proc='d*'" copy=temp/procLangsDBLang now -all
-$MX null count=0 create=$DBLANG now -all
-
-$MX temp/procLangsDBLang append=$DBLANG now -all
-$MX $DBLANG fst=@$PROCLANG_PATH/fst/lang.fst fullinv=$DBLANG
-
--- proc/proc_langs/linux/GeraScilistaCompleta.bat
+++
@@ -1,7 +0,0 @@
-export PATH=$PATH:.
-if [ "@$2" == "@" ] then
-	echo Missing the second parameter: mx 	
-else
-	proc_langs/linux/config/getConfig.bat $2
-	$MX $TITLE lw=9000 "pft=if v50='C' then v68,x1,'$'/ fi" now > $1
-fi
