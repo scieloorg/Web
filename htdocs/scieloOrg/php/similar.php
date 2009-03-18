c397773ac9ee2f9e69836754fa7096fb06702115
fabio.batalha <fabio.batalha@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-03-18 18:44:16 +0000

    limpeza do codigo

--- htdocs/scieloOrg/php/similar.php
+++ htdocs/scieloOrg/php/similar.php
@@ -1,15 +1,7 @@
 <?php
-		$service = "similar";
+	$service = "similar";
 	$xslName = "similar";
 	$serviceName = "similar_SciELO_service";
 	$xpath = "//similar";
 	include_once("common.php");
-
-/*
-		$service = "cited";
-	$xslName = "cited";
-	$serviceName = "cited_SciELO_service";
-	$xpath = "//citing";
-	include("articleServices.php");
-*/
 ?>
