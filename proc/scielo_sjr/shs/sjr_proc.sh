f68b7bc515883ad2982de7e7121f3e8e53830dc8
gustavo.fonseca <gustavo.fonseca@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-04-03 13:34:10 +0000

    Correcao do script

--- proc/scielo_sjr/shs/sjr_proc.sh
+++ proc/scielo_sjr/shs/sjr_proc.sh
@@ -1,8 +1,10 @@
-wget  --output-document=temp.txt 'http://www.scimagojr.com/journalsearch.php?q='$1'&tip=iss&clean=0' 
-#export scimago_id="`cat temp.txt | grep 'input_code.value' | awk '{ print substr( $0, 216, 5 ) }'`"
-export scimago_id="`cat temp.txt | grep 'input_code.value' | awk '{ teste=substr( $0, 216 ); iFinal=index(teste,"+"); print substr( $0, 216,iFinal-2 ) }'`"
-if [ $scimago_id -ne 0 ]
+wget  --output-document=temp.txt 'http://www.scimagojr.com/journalsearch.php?q='$1'&tip=iss&clean=0'
+export scimago_id="`cat temp.txt | grep 'input_code.value' | grep -o 'id=[0-9]*' | grep -o '[0-9][0-9]*'`"
+echo "SCIMAGO_ID: "$scimago_id
+
+if test $scimago_id -gt 0
 then
-	echo '<title ISSN="'$1'" SCIMAGO_ID="'$scimago_id'"/>' >> meuXML.xml
-	exit
+        echo '<title ISSN="'$1'" SCIMAGO_ID="'$scimago_id'"/>' >> meuXML.xml
+else
+        echo -e "This journal does not have SCIMAGO ID: " $1 "\n------------------"
 fi
