522dbd5c45091f1d978e87ccec871668c8cfd525
gustavo.fonseca <gustavo.fonseca@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-04-08 12:50:33 +0000

    Correcao no script

--- proc/scielo_fap/shs/projfapesp_run.sh
+++ proc/scielo_fap/shs/projfapesp_run.sh
@@ -2,7 +2,7 @@
 
 . conf.sh
 
-if [ ! -f /home/scielo/www/bases-devel/projfapesp/projfapesp.mst ] ; then
+if [ ! -f $fapesp_db/projfapesp.mst ] ; then
         $cisis_dir/mx seq=/dev/null create=$fapesp_db/projfapesp
 	cp ../formats/projfapesp.fst $fapesp_db/projfapesp.fst
        #$cisis_dir/mx $fapesp_db/projfapesp fst=@$fapesp_db/projfapesp.fst fullinv=$fapesp_db/projfapesp -all now
@@ -11,19 +11,22 @@ fi
 ######################################################################
 #Remove possiveis arquivos temporarios
 ######################################################################
+echo "REMOVENDO ARQUIVOS TEMPORARIOS"
 rm -rf projfapesp_temp.sh
 rm -rf projfapespdb_temp.seq
 
 ######################################################################
 #Gera Base Temporaria
 ######################################################################
-$cisis_dir/mx iso=$iso_filename "pft=lw(0),(mid(v8[1],instr(v8[1],'S'),23),'|',v900/)" now >> projfapespdb_temp.seq
+echo "GERANDO BASE TEMPORARIA"
+$cisis_dir/mx iso=$iso_filename lw=0 "pft=(mid(v8[1],instr(v8[1],'S'),23),'|',v900/)" now >> projfapespdb_temp.seq
 $cisis_dir/mx seq=projfapespdb_temp.seq create=$fapesp_db/projfapesp_temp -all now
 $cisis_dir/mx $fapesp_db/projfapesp_temp "fst=11 0 v1,v2^*/" fullinv=$fapesp_db/projfapesp_temp 
 
 ######################################################################
 #Gera Base de Projetos
 ######################################################################
+echo "GERANDO BASE DE PROJETOS"
 $cisis_dir/mx null create=$fapesp_db/projfapesp count=0 now
 $cisis_dir/mx iso=$iso_filename create=$filename now -all
 $cisis_dir/mx $filename lw=999999 "pft='./appendpft_aux.sh $cisis_dir/mx $filename ',mfn,' ',mid(v8,instr(v8,'S'),23),' $fapesp_db/projfapesp $fapesp_db/projfapesp_temp'/" now >> projfapesp_temp.sh
@@ -31,6 +34,7 @@ $cisis_dir/mx $filename lw=999999 "pft='./appendpft_aux.sh $cisis_dir/mx $filena
 ######################################################################
 #Executa o script que efetua o append na base
 ######################################################################
+echo "APENDANDO NA BASE"
 chmod +x projfapesp_temp.sh
 ./projfapesp_temp.sh
 
@@ -39,6 +43,7 @@ $cisis_dir/mx $fapesp_db/projfapesp fst=@$fapesp_db/projfapesp.fst fullinv=$fape
 ######################################################################
 #Remove arquivos temporarios
 ######################################################################
+echo "FINALIZANDO"
 rm -rf projfapesp_temp.sh
 rm -rf projfapespdb_temp.seq
 rm -rf ../iso/*mst
