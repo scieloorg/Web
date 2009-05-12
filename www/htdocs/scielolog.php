a9a1807b61483e63268a649fbb3af8ebd279fa6f
fabio.batalha <fabio.batalha@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-05-12 19:45:52 +0000

    ajustando passangem de paramentro por URL para o php

--- www/htdocs/scielolog.php
+++ www/htdocs/scielolog.php
@@ -4,11 +4,23 @@
     // Create new Scielo object
 	$host = $HTTP_HOST;    
 	$scielo = new ScieloLog ($host);
-	
+	$script = $_REQUEST["script"];
+	$dti = $_REQUEST["dti"];
+	$dtf = $_REQUEST["dtf"];
+	$access = $_REQUEST["access"];
+	$cpage = $_REQUEST["cpage"];
+	$nlines = $_REQUEST["nlines"];
+	$pid = $_REQUEST["pid"];
+	$lng = $_REQUEST["lng"];
+	$nrm = $_REQUEST["nrm"];
+	$order = $_REQUEST["order"];
+	$tpages = $_REQUEST["$tpages"];
+	$maccess = $_REQUEST["maccess"];
     $scielo->SetPreferedMethod ("POST");
     
     // Generate wxis url and set xml url
  	$xml = $scielo->GenerateXmlUrl($script,$dti,$dtf,$access,$cpage,$nlines,$pid,$lng,$nrm,$order,$tpages,$maccess);
+
 	$scielo->SetXMLUrl ($xml);
 
     // Generate xsl url and set xsl url
