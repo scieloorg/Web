4a39c9c2eb8ffef25cabbc751cfd9b57c0629336
fabio.batalha <fabio.batalha@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-05-18 14:27:48 +0000

    corrigindo www para home

--- www/proc/scielo_gsc/shs/googleSchoolar_config.sh
+++ www/proc/scielo_gsc/shs/googleSchoolar_config.sh
@@ -1,6 +1,6 @@
 ##################
 # variáveis com caminho para bases de dados utilizadas no processmento.
 ##################
-export database_title="/www/scielo/www/bases/title/title"
-export database_article="/www/scielo/www/bases/artigo/artigo"
-export database_issue="/www/scielo/www/bases/issue/issue"
+export database_title="/home/scielo/www/bases/title/title"
+export database_article="/home/scielo/www/bases/artigo/artigo"
+export database_issue="/home/scielo/www/bases/issue/issue"
