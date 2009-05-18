e453265877e1bb68babb2cfb0d4972ffc95729b1
fabio.batalha <fabio.batalha@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-05-18 14:52:13 +0000

    template

--- www/proc/scielo_gsc/shs/googleSchoolar_config.sh
+++
@@ -1,6 +0,0 @@
-##################
-# variáveis com caminho para bases de dados utilizadas no processmento.
-##################
-export database_title="/home/scielo/www/bases/title/title"
-export database_article="/home/scielo/www/bases/artigo/artigo"
-export database_issue="/home/scielo/www/bases/issue/issue"
