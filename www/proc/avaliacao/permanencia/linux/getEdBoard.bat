ab7e732ad4e5af224d0516d90ab6d489d90b2716
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-05-25 20:16:39 +0000

    git-svn-id: http://svn.reddes.bvsalud.org/scielo-metodologia/trunk@3555 7b3c32f5-e272-4d6f-9936-548b95b19007

--- www/proc/avaliacao/permanencia/linux/getEdBoard.bat
+++ www/proc/avaliacao/permanencia/linux/getEdBoard.bat
@@ -17,11 +17,19 @@ export REL_OUTPUT_PATH=$6
 export HTML_FILE_OUTPUT=$7
 export JournalFile=$HTML_FILE_OUTPUT
 
-$MX "seq=$JOURNAL_EDBOARD_HTML_PATH/$ACRON/$FILENAME¨" lw=999999 "pft=if (v1:'<li>' or v1:'</li>') and not v1:'<a href' then replace(s(replace(replace(v1,'<li>',s(#,'<li>')),'</li>',s(#)),' '),'  ',' ') fi" now> $OUTPUT_PATH/$ACRON/edboard.tmp
-
- 
+# $MX "seq=$JOURNAL_EDBOARD_HTML_PATH/$ACRON/$FILENAME¨" lw=999999 "pft=if (v1:'<li>' or v1:'</li>') and not v1:'<a href' then replace(replace( replace(v1,'<',s(#,'<')) ,'>',s('>',#)),'  ',' ')  fi" now> $OUTPUT_PATH/$ACRON/edboard.tmp
 # echo avaliacao/gizmo/tabEdboard 
-$MX "seq=$OUTPUT_PATH/$ACRON/edboard.tmp¨" lw=999999 gizmo=avaliacao/gizmo/tabEdboard "pft=if p(v1) and size(v1)>2 then v1/ fi" now>  $OUTPUT_PATH/$ACRON/edboard.txt
+# $MX "seq=$OUTPUT_PATH/$ACRON/edboard.tmp¨" lw=999999 gizmo=avaliacao/gizmo/tabEdboard "pft=if p(v1) and size(v1)>2 then v1/ fi" now>  $OUTPUT_PATH/$ACRON/edboard.txt
 
-echo $ACRON CORPO EDITORIAL >> $JournalFile
-more $OUTPUT_PATH/$ACRON/edboard.txt >> $JournalFile
+if [ -f $JOURNAL_EDBOARD_HTML_PATH/$ACRON/$FILENAME ]
+then
+	$MX "seq=$JOURNAL_EDBOARD_HTML_PATH/$ACRON/$FILENAME¨" lw=999999 "pft=if v1:'<' or v1:'>' then replace(replace( replace(v1,'<',s(#,'<')) ,'>',s('>',#)),'  ',' ') fi" now> $OUTPUT_PATH/$ACRON/edboard.tmp
+
+	$MX "seq=$OUTPUT_PATH/$ACRON/edboard.tmp¨" lw=999999 gizmo=avaliacao/gizmo/tabEdboard "pft=if v1:'<' or v1:'>' then ,else v1/ fi" now>  $OUTPUT_PATH/$ACRON/edboard.txt
+	echo $ACRON CORPO EDITORIAL >> $JournalFile
+	more $OUTPUT_PATH/$ACRON/edboard.txt >> $JournalFile
+
+fi
+
+
+
