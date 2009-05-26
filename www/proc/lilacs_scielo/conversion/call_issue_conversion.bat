06d73e1153e479a8719bf1fcc9de1875ab100af2
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-05-26 21:37:11 +0000

    git-svn-id: http://svn.reddes.bvsalud.org/scielo-metodologia/trunk@3577 7b3c32f5-e272-4d6f-9936-548b95b19007

--- www/proc/lilacs_scielo/conversion/call_issue_conversion.bat
+++ www/proc/lilacs_scielo/conversion/call_issue_conversion.bat
@@ -1,10 +1,9 @@
 @echo off
 
-if not exist %serial_path%\%1\%2\base\%2.mst mkdir %serial_path%\%1\%2\base
-
+echo  %serial_path%\%1\%2\base
+if not exist %serial_path%\%1\%2\base mkdir %serial_path%\%1\%2\base
 %mx% null count=0 create=%serial_path%\%1\%2\base\%2 now -all
-
-
+if errorlevel==1 pause
 %wxis% IsisScript=lilacs_scielo\conversion\generateSciELOfromLILACS.xis reprocess=%3 center=%center% acron=%1 issueid=%2 proc_path=%proc_path% cip=%cipar_file% debug=%debug% serial_path=%serial_path% url=%url% >> %log_file%
 
 
