3c6e7c019b3b2f73b3d766fe218dabd11f58c3d7
fabio.batalha <fabio.batalha@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-07-03 18:06:25 +0000

    scieloUpdate

--- proc/scieloUpdate/scieloUpdate_config.sh
+++
@@ -1,7 +0,0 @@
-# Arquivo de Configuracao
-# Endereco do Repositorio
-svnLocal='svn://reddes.bvsalud.org:33690/scielo-metodologia'
-# Caminho da aplicacao
-caminhoAPL='/home/scielo/'
-# branches
-branch=desenvolvimento/fabio.batalha
