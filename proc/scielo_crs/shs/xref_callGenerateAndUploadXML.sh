4628d172f1ae6dc324a1e66e80523fd3915f6de1
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-07-07 21:03:39 +0000

    scielo_crs - tratamento de VERBA

--- proc/scielo_crs/shs/xref_callGenerateAndUploadXML.sh
+++ proc/scielo_crs/shs/xref_callGenerateAndUploadXML.sh
@@ -5,14 +5,14 @@
 #
 . crossref_config.sh
 
-
-ISSN_OR_PID=$1
+PROCESS_ONLY_NEW=$1
+ISSN_OR_PID=$2
 
 if [ "@$ISSN_OR_PID" == "@" ]
 then
-	$cisis_dir/mx cipar=$MYCIPFILE ARTIGO_DB "btell=0" "hr=$" lw=9999 "pft='$conversor_dir/shs/xref_generateAndUploadXML.sh ',if p(v881) then v881, else v880 fi/" now > $MYTEMP/x.sh
+	$cisis_dir/mx cipar=$MYCIPFILE ARTIGO_DB "btell=0" "hr=$" lw=9999 "pft='$conversor_dir/shs/xref_generateAndUploadXML.sh ',if p(v881) then v881, else v880 fi,' DO',ref(['XREF_DOI_REPORT']l(['XREF_DOI_REPORT']'hr=',if p(v881) then v881, else v880 fi),if v30<>'error' then 'NOT' fi),' $PROCESS_ONLY_NEW'/" now > $MYTEMP/x.sh
 else
-	$cisis_dir/mx cipar=$MYCIPFILE ARTIGO_DB "btell=0" "hr=S$ISSN_OR_PID$ or hr=$ISSN_OR_PID$" lw=9999 "pft='$conversor_dir/shs/xref_generateAndUploadXML.sh ',if p(v881) then v881, else v880 fi/" now > $MYTEMP/x.sh
+	$cisis_dir/mx cipar=$MYCIPFILE ARTIGO_DB "btell=0" "hr=S$ISSN_OR_PID$ or hr=$ISSN_OR_PID$" lw=9999 "pft='$conversor_dir/shs/xref_generateAndUploadXML.sh ',if p(v881) then v881, else v880 fi,' DO',ref(['XREF_DOI_REPORT']l(['XREF_DOI_REPORT']'hr=',if p(v881) then v881, else v880 fi),if v30<>'error' then 'NOT' fi),' $PROCESS_ONLY_NEW'/" now > $MYTEMP/x.sh
 fi
 
 chmod 775 $MYTEMP/x.sh
--- proc/scielo_crs/shs/xref_prepareEnv.sh
+++ proc/scielo_crs/shs/xref_prepareEnv.sh
@@ -3,6 +3,6 @@
 if [ ! -f $XREF_DOI_REPORT.mst ]
 then
 	$cisis_dir/mx null count=1 "proc='a99{',date,'{'" create=$XREF_DOI_REPORT now -all
-	$cisis_dir/mx $XREF_DOI_REPORT fst=@$XREF_DOI_REPORT.fst fullinv=$XREF_DOI_REPORT 
+	$cisis_dir/mx $XREF_DOI_REPORT fst=@$conversor_dir/fst/crossref_DOIReport.fst fullinv=$XREF_DOI_REPORT 
 fi
 
