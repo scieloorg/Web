b368419a32950c2fc650b5d5a4c62f3ed7a38e11
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-07-07 21:03:39 +0000

    scielo_crs - tratamento de VERBA

--- www/proc/scielo_crs/shs/xref_run_budget.sh
+++ www/proc/scielo_crs/shs/xref_run_budget.sh
@@ -9,68 +9,121 @@
 . crossref_config.sh
 chmod 775 *.sh
 
-ORDER=$1
+BUDGETID=$1
+ORDER=$2
+FULL=$3
+# use only for while testing
+RESET=$4 
 
 SORTEDLIST=$MYTEMP/list
 
-echo Este procesamiento generara un listado de PID para ser usado en el deposito de CrossRef
-echo Considerando
-echo BASE ARTIGO en ARTIGO_DB
+echo Este procesamiento hara un deposito de XML en CrossRef
+echo 
+echo Lee con atencion la configuracion que sera presentada a seguir
+echo 
+echo ENTER para seguir o CTRL+C para interrumpir
+read
+clear
+echo Este procesamiento hara un deposito de XML en CrossRef considerando
+echo 
+echo BASE ARTIGO en $ARTIGO_DB
 echo cisis en $cisis_dir
-echo ARTICULOS RECIENTES DESDE EL ANO $FIRST_YEAR_OF_RECENT_FEE
-echo VALOR PARA ESTES ARTICULOS \$ $RECENT_FEE
-echo VALOR PARA ARTICULOS ANTERIORES A $FIRST_YEAR_OF_RECENT_FEE \$ $BACKFILES_FEE
-echo VALOR DISPONIBLE PARA EL PAGO $BUDGET
-
 echo
-if [ "$2" == "reseting" ]
+if [ "@$ORDER" == "@Descending" ]
+then
+	echo ARTICULOS MAS RECIENTES PARA LOS MAS ANTIGUOS
+else
+	if [ "@$ORDER" == "@Ascending" ]
+	then
+		echo ARTICULOS MAS ANTIGUOS PARA LOS MAS RECIENTES
+	else
+		echo Informe el valor para ORDER Descending o Ascending
+	fi
+fi
+echo 
+echo VALOR PARA ARTICULOS DESDE DE     $FIRST_YEAR_OF_RECENT_FEE \$ $RECENT_FEE
+echo VALOR PARA ARTICULOS ANTERIORES A $FIRST_YEAR_OF_RECENT_FEE \$ $BACKFILES_FEE
+echo PARA EL PAGO SERA USADO EL PRESUPUESTO CUYO ID ES $BUDGETID
+
+#echo 
+#echo Edite el archivo db_presupuestos.txt
+#read
+#vi db_presupuestos.txt
+#echo ENTER para seguir y CTRL+C para interrumpir
+
+###################
+# PREPARE THE ENVIROMENT
+#
+if [ "$RESET" == "reseting" ]
 then
 	$conversor_dir/shs/xref_reseting.sh
 fi
-$conversor_dir/shs/xref_prepareEnvBudget.sh
+$conversor_dir/shs/xref_prepareEnvBudget.sh $BUDGETID
+
+$cisis_dir/mx cipar=$MYCIPFILE DB_PRESUPUESTOS btell=0 "$BUDGETID" "pft='fecha del presupuesto: 'v3/,'valor: ',v2/" now 
+echo
+BATCHBGID=`cat temp`
+echo Batch id $BATCHBGID
+
+echo Estando correcta la configuracion ENTER para seguir
+echo Estando incorrecta CTRL+C para interrumpir
+echo  editar el crossref_config.sh para corregir la configuracion
+
+read
 
 if [ -f $SORTEDLIST.txt ]
 then
 	mv $SORTEDLIST.txt $MYTEMP/lixo
 fi
 
+###################
+# GENERATE THE SORTED LIST
+#
 
 if [ "@$ORDER" == "@Descending" ]
 then
-	echo ARTICULOS MAS RECIENTES PARA LOS MAS ANTIGUOS
-	$cisis_dir/mx cipar=$MYCIPFILE ARTIGO_DB btell=0  "tp=h" "proc='a9001{$FIRST_YEAR_OF_RECENT_FEE{a9002{$RECENT_FEE{a9003{$BACKFILES_FEE{a9005{$BUDGETID{'" lw=9999 "pft=@$conversor_dir/pft/xref_generateList.pft" now | sort -u -r > $SORTEDLIST.txt
+	
+	$cisis_dir/mx cipar=$MYCIPFILE ARTIGO_DB btell=0  "tp=h" "proc='a9001{$FIRST_YEAR_OF_RECENT_FEE{a9002{$RECENT_FEE{a9003{$BACKFILES_FEE{a9005{$BUDGETID{a9055{$FULL{'" lw=9999 "pft=@$conversor_dir/pft/xref_generateList.pft" now | sort -u -r > $SORTEDLIST.txt
 else
 	if [ "@$ORDER" == "@Ascending" ]
 	then
-		echo ARTICULOS MAS ANTIGUOS PARA LOS MAS RECIENTES
-		$cisis_dir/mx cipar=$MYCIPFILE ARTIGO_DB btell=0  "tp=h" "proc='a9001{$FIRST_YEAR_OF_RECENT_FEE{a9002{$RECENT_FEE{a9003{$BACKFILES_FEE{a9005{$BUDGETID{'" lw=9999 "pft=@$conversor_dir/pft/xref_generateList.pft" now | sort -u > $SORTEDLIST.txt
+		
+		$cisis_dir/mx cipar=$MYCIPFILE ARTIGO_DB btell=0  "tp=h" "proc='a9001{$FIRST_YEAR_OF_RECENT_FEE{a9002{$RECENT_FEE{a9003{$BACKFILES_FEE{a9005{$BUDGETID{a9055{$FULL{'" lw=9999 "pft=@$conversor_dir/pft/xref_generateList.pft" now | sort -u > $SORTEDLIST.txt
 	else
-		echo Informe el valor para ORDER Descending o Ascending
+		$cisis_dir/mx cipar=$MYCIPFILE ARTIGO_DB btell=0  "tp=h" "proc='a9001{$FIRST_YEAR_OF_RECENT_FEE{a9002{$RECENT_FEE{a9003{$BACKFILES_FEE{a9005{$BUDGETID{a9055{$FULL{'" lw=9999 "pft=@$conversor_dir/pft/xref_generateList.pft" now | sort -u -r > $SORTEDLIST.txt
 	fi
 fi
 
-echo Para seguir ENTER 
-echo Para interrumpir CTRL+C
-read
-
+###################
+# EXECUTE
+#
 if [ -f $SORTEDLIST.txt ]
 then
-	$cisis_dir/mx cipar=$MYCIPFILE null count=0 create=$DB_BILL now -all
-	
-	$cisis_dir/mx cipar=$MYCIPFILE ARTIGO_DB btell=0 "tp=h" "proc='d*a880{',v880,'{',|a881{|v881|{|,|a223{|v223|{|,'a65{',v65,'{',if l(['DB_BILL']if p(v881) then v881 else v880 fi)>0 then ref(['DB_BILL']l(['DB_BILL']if p(v881) then v881 else v880 fi), |a1{|v1|{|,|a2{|v2|{|,|a3{|v3|{|,|a4{|v4|{|) fi" append=$DB_BILL now -all
+	$cisis_dir/mx cipar=$MYCIPFILE null count=0 create=DB_BILL_BKP now -all
+		
+	$cisis_dir/mx cipar=$MYCIPFILE ARTIGO_DB btell=0 "tp=h" "proc='d*a880{',v880,'{',|a881{|v881|{|,|a223{|v223|{|,'a65{',v65,'{',if l(['DB_BILL']if p(v881) then v881 else v880 fi)>0 then ref(['DB_BILL']l(['DB_BILL']if p(v881) then v881 else v880 fi), |a1{|v1|{|,|a2{|v2|{|,|a3{|v3|{|,|a4{|v4|{|) fi" append=DB_BILL_BKP now -all
+
+	$cisis_dir/mx cipar=$MYCIPFILE DB_BILL_BKP create=DB_BILL now -all
 	$cisis_dir/mx $DB_BILL fst=@$conversor_dir/fst/bill.fst fullinv=$DB_BILL now -all
 
 	# $cisis_dir/mx "seq=$SORTEDLIST.txt " create=$SORTEDLIST now -all
-	# $cisis_dir/mx cipar=$MYCIPFILE $SORTEDLIST lw=99999 "pft='$conversor_dir/shs/xref_run_budget_calculateFee.sh ',v1,' ',v2,' ',v3,' $cisis_dir $MYCIPFILE $BUDGETID ',ref(['DB_BG']s('$BUDGETID'),v2)/" now > $MYTEMP/xref_run_budget_calculateFee.sh
+	# $cisis_dir/mx cipar=$MYCIPFILE $SORTEDLIST lw=99999 "pft='$conversor_dir/shs/xref_run_budget_calculateFee.sh ',v1,' ',v2,' ',v3,' $cisis_dir $MYCIPFILE $BUDGETID ',ref(['DB_PRESUPUESTOS']s('$BUDGETID'),v2)/" now > $MYTEMP/xref_run_budget_calculateFee.sh
 
 
-	$cisis_dir/mx cipar=$MYCIPFILE "seq=$SORTEDLIST.txt " lw=99999 "pft='$conversor_dir/shs/xref_run_budget_calculateFee.sh ',v1,' ',v2,' ',v3,' $cisis_dir $MYCIPFILE $BUDGETID ',ref(['DB_BG']l(['DB_BG']'$BUDGETID'),v2),' ',mfn/" now > $MYTEMP/xref_run_budget_calculateFee.sh
+	$cisis_dir/mx cipar=$MYCIPFILE "seq=$SORTEDLIST.txt " lw=99999 "pft='$conversor_dir/shs/xref_run_budget_calculateFee.sh ',v1,' ',v2,' ',v3,' $cisis_dir $MYCIPFILE $BUDGETID ',ref(['DB_PRESUPUESTOS']l(['DB_PRESUPUESTOS']'$BUDGETID'),v2),' ',mfn,' $BATCHBGID'/" now > $MYTEMP/xref_run_budget_calculateFee.sh
 
 	chmod 775 $MYTEMP/xref_run_budget_calculateFee.sh
 	$MYTEMP/xref_run_budget_calculateFee.sh
 	
-	$cisis_dir/mx cipar=$MYCIPFILE DB_BG btell=0 "bool=$BUDGETID" lw=9999 "pft=@$conversor_dir/pft/budget_report.pft" now 
+	
+	$cisis_dir/mx cipar=$MYCIPFILE DB_BATCH_RUN_BUDGET btell=0 "bool=BATCHBG=$BATCHBGID" "proc='a200{$FIRST_YEAR_OF_RECENT_FEE{a201{$RECENT_FEE{a202{$BACKFILES_FEE{'" copy=DB_BATCH_RUN_BUDGET now -all
 
+	echo #######################
+	echo
+	echo   REPORT of BUDGET ID=$BUDGETID 
+	echo 
+	$cisis_dir/mx cipar=$MYCIPFILE DB_BATCH_RUN_BUDGET btell=0 "bool=$BUDGETID" lw=9999  "pft=@$conversor_dir/pft/budget_report.pft" now 
+	
 fi
 ###
 # DB_BG
--- www/proc/scielo_crs/shs/xref_run_budget_calculateFee.sh
+++ www/proc/scielo_crs/shs/xref_run_budget_calculateFee.sh
@@ -12,44 +12,46 @@ BUDGETID=$6
 
 MYBUDGET=$7
 PRIOR=$8
+BATCHBGID=$9
 
 if [ -f $MYTEMP/WHATTODO ]
 then
 	rm $MYTEMP/WHATTODO
 fi
 
-echo VERIFICA BUDGET
-$MYCISISDIR/mx cipar=$MYCIPFILE null count=1 "pft='saldo:  ',ref(['DB_BG']l(['DB_BG']'$BUDGETID'),v4)/,'budget: ',f(val('$MYBUDGET'),1,2)/,'saldo + taxa: ',f( val(ref(['DB_BG']l(['DB_BG']'$BUDGETID'),v4)) + val('$FEE'),1,2)/" now 
-$MYCISISDIR/mx cipar=$MYCIPFILE null count=1 "pft=if val('$MYBUDGET')>=(val(ref(['DB_BG']l(['DB_BG']'$BUDGETID'),v4)) + val('$FEE')) then 'doit' else 'dont' fi/" now
+# echo VERIFICA BUDGET
+# $MYCISISDIR/mx cipar=$MYCIPFILE null count=1 "pft='saldo:  ',ref(['DB_CTRL_BG']l(['DB_CTRL_BG']'$BUDGETID'),v2)/,'budget: ',f(val('$MYBUDGET'),1,2)/,'saldo + taxa: ',f( val(ref(['DB_CTRL_BG']l(['DB_CTRL_BG']'$BUDGETID'),v2)) + val('$FEE'),1,2)/" now 
 #read
 
-$MYCISISDIR/mx cipar=$MYCIPFILE null count=1 "pft=if val('$MYBUDGET')>=(val(ref(['DB_BG']l(['DB_BG']'$BUDGETID'),v4)) + val('$FEE')) then 'doit' else 'dont' fi" now > $MYTEMP/WHATTODO
-
+$MYCISISDIR/mx cipar=$MYCIPFILE null count=1 "pft=if val('$MYBUDGET')>=(val(ref(['DB_CTRL_BG']l(['DB_CTRL_BG']'$BUDGETID'),v2)) + val('$FEE')) then 'doit' else 'dont' fi/" now > $MYTEMP/WHATTODO
 WHATTODO=`cat $MYTEMP/WHATTODO`
 
+#echo $WHATTODO
 
 if [ "$WHATTODO" == "doit" ]
 then
-	$conversor_dir/shs/xref_run.sh $PID $PRIOR
+	$conversor_dir/shs/xref_run.sh $PID
 
 	$cisis_dir/mx $XREF_DOI_REPORT btell=0 "bool=hr=$PID" "pft=if p(v30) then ,'done'/, fi" now > $MYTEMP/WHATTODO
 	WHATTODO=`cat $MYTEMP/WHATTODO`
 
 	if [ "$WHATTODO" == "done" ]
 	then
-		$MYCISISDIR/mx cipar=$MYCIPFILE DB_BILL btell=0 "bool=$PID" count=1 "proc='d4a4{requested{a2{$FEE{a3{',date,'{a1{$BUDGETID{a121{$PRIOR{'" copy=DB_BILL now -all
+		$MYCISISDIR/mx cipar=$MYCIPFILE DB_BILL btell=0 "bool=$PID" count=1 "proc='d4d100d121a4{requested{a2{$FEE{a3{',date,'{a1{$BUDGETID{a121{$PRIOR{a100{$BATCHBGID{'" copy=DB_BILL now -all
 		$cisis_dir/mx $DB_BILL fst=@$conversor_dir/fst/bill.fst fullinv=$DB_BILL now -all
 
-		echo Novo saldo
-		$MYCISISDIR/mx cipar=$MYCIPFILE DB_BG btell=0 "bool=$BUDGETID" "pft=f(val(v4)+val('$FEE'),1,2)/" now
+		#echo Novo saldo
+		#$MYCISISDIR/mx cipar=$MYCIPFILE DB_CTRL_BG btell=0 "bool=$BUDGETID" "pft=f(val(v2)+val('$FEE'),1,2)/" now
 		#read
-		$MYCISISDIR/mx cipar=$MYCIPFILE DB_BG btell=0 "bool=$BUDGETID" "proc='d121d4a4{',f(val(v4)+val('$FEE'),1,2),'{a121{$PRIOR{'" copy=DB_BG now -all
+		$MYCISISDIR/mx cipar=$MYCIPFILE DB_CTRL_BG btell=0 "bool=$BUDGETID" "proc='d2d90a2{',f(val(v2)+val('$FEE'),1,2),'{a90{',date,'{'" copy=DB_CTRL_BG now -all
+
+		$MYCISISDIR/mx cipar=$MYCIPFILE DB_BATCH_RUN_BUDGET btell=0 "bool=BATCHBG=$BATCHBGID" "proc='d2d90d121a121{$PRIOR{a2{',f(val(v2)+val('$FEE'),1,2),'{a90{',date,'{'" copy=DB_BATCH_RUN_BUDGET now -all
 	fi
 else
-	$MYCISISDIR/mx cipar=$MYCIPFILE DB_BILL btell=0 "bool=$PID" count=1 "proc='d121d4a4{dont{a2{$FEE{a3{',date,'{a1{$BUDGETID{a121{$PRIOR{'" copy=DB_BILL now -all	
+	$MYCISISDIR/mx cipar=$MYCIPFILE DB_BILL btell=0 "bool=$PID" count=1 "proc='d4d100d121a4{dont{a2{$FEE{a3{',date,'{a1{$BUDGETID{a121{$PRIOR{a100{$BATCHBGID{'" copy=DB_BILL now -all	
 	$cisis_dir/mx $DB_BILL fst=@$conversor_dir/fst/bill.fst fullinv=$DB_BILL now -all
 
-	$MYCISISDIR/mx cipar=$MYCIPFILE DB_BG btell=0 "bool=$BUDGETID" "proc='d122d10a122{$PRIOR{a10{',f(val(v10)+val('$FEE'),1,2),'{'" copy=DB_BG now -all
+	$MYCISISDIR/mx cipar=$MYCIPFILE DB_BATCH_RUN_BUDGET btell=0 "bool=BATCHBG=$BATCHBGID" "proc='d90d122d10a122{$PRIOR{a10{',f(val(v10)+val('$FEE'),1,2),'{a90{',date,'{'" copy=DB_BATCH_RUN_BUDGET now -all
 
 fi
 
