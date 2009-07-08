f13222e83d9a0cf0c9f17cf301234d20237a763f
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-07-08 14:29:22 +0000

    git-svn-id: http://svn.reddes.bvsalud.org/scielo-metodologia/trunk@3661 7b3c32f5-e272-4d6f-9936-548b95b19007

---
+++ www/proc/scielo_crs/shs/xref_run_budget_shortcut.sh
@@ -0,0 +1,7 @@
+#
+# Editar db_presupuestos.txt
+# Editar crossref_config.sh
+
+# Parameter 1 BUDGETID
+# Parameter 2 ORDER Descending for more recent to older or Ascending for older to more recent
+./xref_run_budget.sh 1 Descending 
