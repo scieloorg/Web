59754aff45a49edb19e28b7248c5a4dde4c87e8f
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-07-15 15:52:54 +0000

    scielo_crs - tratamento de VERBA - melhoria - interrompe o processamento quando atinge a verba. Anteriormente, mesmo que os artigos que ultrapassassem a verba, ainda que não submetidos, eram  processados para seus dados serem registrados, pois ao final poderia se ter a idéia de quanto faltaria para futuras submissões. No entanto, isso poderia ficar custoso ao servidor.
    git-svn-id: http://svn.reddes.bvsalud.org/scielo-metodologia/trunk@3673 7b3c32f5-e272-4d6f-9936-548b95b19007

--- www/proc/scielo_crs/shs/xref_run_budget_calculateFee.sh
+++ www/proc/scielo_crs/shs/xref_run_budget_calculateFee.sh
@@ -1,61 +1,17 @@
 . crossref_config.sh
 
 # taxa para artigos recentes
-PUBDATE=$1
-PID=$2
-FEE=$3
+FEE=$1
+BUDGETID=$2
 
-MYCISISDIR=$4
-MYCIPFILE=$5
-
-BUDGETID=$6
-
-MYBUDGET=$7
-PRIOR=$8
-BATCHBGID=$9
 
 if [ -f $MYTEMP/WHATTODO ]
 then
 	rm $MYTEMP/WHATTODO
 fi
+$cisis_dir/mx cipar=$MYCIPFILE DB_PRESUPUESTOS btell=0 "bool=$BUDGETID" "pft='budget: ',v2/" now
+$cisis_dir/mx cipar=$MYCIPFILE DB_CTRL_BG btell=0 "bool=$BUDGETID" "pft='saldo: ',v2/" now
+echo $FEE
 
-# echo VERIFICA BUDGET
-# $MYCISISDIR/mx cipar=$MYCIPFILE null count=1 "pft='saldo:  ',ref(['DB_CTRL_BG']l(['DB_CTRL_BG']'$BUDGETID'),v2)/,'budget: ',f(val('$MYBUDGET'),1,2)/,'saldo + taxa: ',f( val(ref(['DB_CTRL_BG']l(['DB_CTRL_BG']'$BUDGETID'),v2)) + val('$FEE'),1,2)/" now 
-#read
-
-$MYCISISDIR/mx cipar=$MYCIPFILE null count=1 "pft=if val('$MYBUDGET')>=(val(ref(['DB_CTRL_BG']l(['DB_CTRL_BG']'$BUDGETID'),v2)) + val('$FEE')) then 'doit' else 'dont' fi/" now > $MYTEMP/WHATTODO
-WHATTODO=`cat $MYTEMP/WHATTODO`
-
-#echo $WHATTODO
-
-if [ "$WHATTODO" == "doit" ]
-then
-	$conversor_dir/shs/xref_run.sh $PID
-
-	$cisis_dir/mx $XREF_DOI_REPORT btell=0 "bool=hr=$PID" "pft=if p(v30) then ,'done'/, fi" now > $MYTEMP/WHATTODO
-	WHATTODO=`cat $MYTEMP/WHATTODO`
-
-	if [ "$WHATTODO" == "done" ]
-	then
-		$MYCISISDIR/mx cipar=$MYCIPFILE DB_BILL btell=0 "bool=$PID" count=1 "proc='d*'" copy=DB_BILL now -all
-		$MYCISISDIR/mx cipar=$MYCIPFILE null count=1 "proc=ref(['ARTIGO_DB']l(['ARTIGO_DB']'hr=$PID'),'a880{',v880,'{',|a881{|v881|{|,|a223{|v223|{|,'a65{',v65,'{'),'a4{requested{a2{$FEE{a3{',date,'{a1{$BUDGETID{a121{$PRIOR{a100{$BATCHBGID{a30{',ref(['XREF_DOI_REPORT']l(['XREF_DOI_REPORT']'hr=$PID'),v30,|^x|v930),'{'" append=DB_BILL now -all
-		$cisis_dir/mx $DB_BILL fst=@$conversor_dir/fst/bill.fst fullinv=$DB_BILL now -all
-
-		#echo Novo saldo
-		#$MYCISISDIR/mx cipar=$MYCIPFILE DB_CTRL_BG btell=0 "bool=$BUDGETID" "pft=f(val(v2)+val('$FEE'),1,2)/" now
-		#read
-		$MYCISISDIR/mx cipar=$MYCIPFILE DB_CTRL_BG btell=0 "bool=$BUDGETID" "proc='d2d90a2{',f(val(v2)+val('$FEE'),1,2),'{a90{',date,'{'" copy=DB_CTRL_BG now -all
-
-		$MYCISISDIR/mx cipar=$MYCIPFILE DB_BATCH_RUN_BUDGET btell=0 "bool=BATCHBG=$BATCHBGID" "proc='d2d90d121a121{$PRIOR{a2{',f(val(v2)+val('$FEE'),1,2),'{a90{',date,'{'" copy=DB_BATCH_RUN_BUDGET now -all
-	fi
-else
-	$MYCISISDIR/mx cipar=$MYCIPFILE DB_BILL btell=0 "bool=$PID" count=1 "proc='d4d100d121a4{dont{a2{$FEE{a3{',date,'{a1{$BUDGETID{a121{$PRIOR{a100{$BATCHBGID{'" copy=DB_BILL now -all	
-	$cisis_dir/mx $DB_BILL fst=@$conversor_dir/fst/bill.fst fullinv=$DB_BILL now -all
-
-	$MYCISISDIR/mx cipar=$MYCIPFILE DB_BATCH_RUN_BUDGET btell=0 "bool=BATCHBG=$BATCHBGID" "proc='d90d122d10a122{$PRIOR{a10{',f(val(v10)+val('$FEE'),1,2),'{a90{',date,'{'" copy=DB_BATCH_RUN_BUDGET now -all
-
-fi
-
-
-
+$cisis_dir/mx cipar=$MYCIPFILE DB_PRESUPUESTOS btell=0 "bool=$BUDGETID" "pft=if val(v2)>=(val(ref(['DB_CTRL_BG']l(['DB_CTRL_BG']'$BUDGETID'),v2)) + val('$FEE')) then 'doit' else 'dont' fi/" now > $MYTEMP/WHATTODO
 
