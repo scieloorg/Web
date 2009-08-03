1f54db746c611422e73ccdf1ef53e4dc37535885
marcio.galvao <marcio.galvao@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-08-03 18:29:28 +0000

    #119

--- htdocs/scielolog/updateLog02.php
+++ htdocs/scielolog/updateLog02.php
@@ -5,10 +5,18 @@ $hojeDia=date("d");
 $hojeHora=date("H");
 $hojeMin=date("i");
 
+$pid=$_REQUEST['pid'];
+$app=$_REQUEST['app'];
+$page=$_REQUEST['page'];
+$issn=$_REQUEST['issn'];
+$lang=$_REQUEST['lang'];
+$norm=$_REQUEST['norm'];
+$tlng=$_REQUEST['tlng'];
+
 $ip = getenv("REMOTE_ADDR"); 
 
 $defFile = parse_ini_file(dirname(__FILE__)."/../scielo.def.php","true");
-$base="$TRAB/log".$hojeAno.$hojeMes.$hojeDia;
+$base="\$TRAB/log".$hojeAno.$hojeMes.$hojeDia;
 $filename=$defFile["LOG"]["ACCESSSTAT_LOG_DIRECTORY"]."/program_log".$hojeAno.$hojeMes.$hojeDia.".txt";
    
 // Split PID into ISSN and ORD
@@ -25,7 +33,7 @@ $ord  = substr($pidUP,$init_pos);
 $data=date("Y").$hojeMes.$hojeDia.$hojeHora.$hojeMin;
 $hostname = gethostbyaddr($ip);
 $prc="\"proc='<1 0>$app</1><2 0>$page</2><3 0>$issn</3><4 0>$lang</4><5 0>$norm</5>";
-$prc=$prc."<6 0>$data</6><7 0>$ip</7><8 0>$ord</8><9 0>$hostname</9>',\"";
+$prc=$prc."<6 0>$data</6><7 0>$ip</7><8 0>$ord</8><9 0>$hostname</9><10 0>$tlng</0>',\"";
 $OP1="\$UTL/mx null count=1 append=$base $prc";
 
 $handle = fopen($filename, 'a');
