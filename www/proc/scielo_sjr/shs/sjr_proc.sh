8375f98045ef7cff34cc50d486c87dda9a42e8bc
fabio.santos <fabio.santos@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-09-23 18:17:21 +0000

    #159 Mudando esquema do scimago para ler imagens carregadas em file system.
    git-svn-id: http://svn.reddes.bvsalud.org/scielo-metodologia/trunk@3780 7b3c32f5-e272-4d6f-9936-548b95b19007

--- www/proc/scielo_sjr/shs/sjr_proc.sh
+++ www/proc/scielo_sjr/shs/sjr_proc.sh
@@ -2,8 +2,10 @@ wget  --output-document=temp.txt 'http://www.scimagojr.com/journalsearch.php?q='
 export scimago_id="`cat temp.txt | grep 'input_code.value' | grep -o 'id=[0-9]*' | grep -o '[0-9][0-9]*'`"
 echo "SCIMAGO_ID: "$scimago_id
 
+mkdir -p images/
 if test $scimago_id -gt 0
 then
+        wget -O images/$1.gif http://www.scimagojr.com/journal_img.php?id=$scimago_id&title=false
         echo '<title ISSN="'$1'" SCIMAGO_ID="'$scimago_id'"/>' >> meuXML.xml
 else
         echo -e "This journal does not have SCIMAGO ID: " $1 "\n------------------"
