b91e8841fd8d09a7ff0adc8c334d33d2f1bd1966
marcio.galvao <marcio.galvao@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-10-20 11:55:07 +0000

    #176
    git-svn-id: http://svn.reddes.bvsalud.org/scielo-metodologia/trunk@3823 7b3c32f5-e272-4d6f-9936-548b95b19007

--- www/proc/Envia2LogAcesso.sh
+++ www/proc/Envia2LogAcesso.sh
@@ -9,30 +9,31 @@ export seg=`date +%S`
 
 export WRK=$1/bases/accesslog/log_scielo/work
  
-rem ===== Aumentar o espaco de variaveis de ambiente
+rem  ===== Aumentar o espaco de variaveis de ambiente
 rem CONFIG.SYS
-rem 
+rem   
 rem Envia2LogAcesso
 rem Parametro 1: path da producao da SciELO
 rem Parametro 2: arquivo com instrucoes de FTP
 rem Parametro 3: arquivo de log
 
-
 rem Inicializa variaveis
 
 export INFORMALOG=log/Envia2LogAcesso.log
-export CISIS_DIR=cisis
+export CISIS_DIR=/home/scielo_prod/www/proc/cisis
 
-rem Verifica parametros
-call batch/VerifPresencaParametro.bat $0 @$1 path producao SciELO
-call batch/VerifPresencaParametro.bat $0 @$2 arquivo com instrucoes de FTP
-call batch/VerifExisteArquivo.bat $2
-call batch/VerifPresencaParametro.bat $0 @$3 arquivo de LOG
+rem  Verifica parametros
 
-call batch/InformaLog.bat $0 dh ===Inicio===
-
-call batch/CriaDiretorio.bat temp/transf2logacesso
-
+ call batch/VerifPresencaParametro.bat $0 @$1 path producao SciELO
+ call batch/VerifPresencaParametro.bat $0 @$2 arquivo com instrucoes de FTP
+ call batch/VerifExisteArquivo.bat $2
+ call batch/VerifPresencaParametro.bat $0 @$3 arquivo de LOG
+ 
+ call batch/InformaLog.bat $0 dh ===Inicio===
+ 
+ call batch/CriaDiretorio.bat temp/transf2logacesso
+ 
+ cd $1/www/proc 
 
 if [ -d $1/bases/accesslog/log_scielo/ ]
     then
@@ -44,7 +45,7 @@ if [ -d $1/bases/accesslog/log_scielo/ ]
 	$CISIS_DIR/mx seq=lista.seq lw=999 "pft='basename ',v1,' >> lista2.seq',#"  -all now > prog01.sh
 	chmod 777 prog01.sh
 	./prog01.sh
-	$CISIS_DIR/mx seq=lista2.seq lw=999 "pft='mv $1/bases/accesslog/log_scielo/',v1,'.txt temp/transf2logacesso/',v1,'$hora$min$seg','.txt'/" -all now > prog02.sh
+	$CISIS_DIR/mx seq=lista2.seq lw=999 "pft='mv $1/bases/accesslog/log_scielo/',v1,'.txt $1/proc/temp/transf2logacesso/',v1,'$hora$min$seg','.txt'/" -all now > prog02.sh
 	chmod 777 prog02.sh
 	./prog02.sh      
 
@@ -57,11 +58,11 @@ if [ -d $1/bases/accesslog/log_scielo/ ]
 
 	ftp -i -v -n < temp/ENVIA2ACESSOFTP.txt >> $INFORMALOG
  
- 	call batch/CriaDiretorio.bat $WRK/$ano$mes$dia$hora$min
- 	
+	call batch/CriaDiretorio.bat $WRK/$ano$mes$dia$hora$min
+	
 	mv temp/transf2logacesso/*.txt $WRK/$ano$mes$dia$hora$min	
 
-	batch/ifErrorLevel.bat $? batch/AchouErro.bat $0 ftp: $2
+	call batch/ifErrorLevel.bat $? call batch/AchouErro.bat $0 ftp: $2
 
 	call batch/InformaLog.bat $0 dh ===Fim=== LOG gravado em: $INFORMALOG
 fi
