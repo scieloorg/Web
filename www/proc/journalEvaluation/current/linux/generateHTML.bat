9a60bf6b44421cd0d266ad5fe1132cd5d3813ad4
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-11-06 20:30:25 +0000

    processamento de avaliacao de periodicos de permanencia 200910
    git-svn-id: http://svn.reddes.bvsalud.org/scielo-metodologia/trunk@3847 7b3c32f5-e272-4d6f-9936-548b95b19007

--- www/proc/journalEvaluation/current/linux/generateHTML.bat
+++ www/proc/journalEvaluation/current/linux/generateHTML.bat
@@ -4,6 +4,6 @@ export OUTPUT_HTML=$3
 
 $MX null count=1 "pft='<html><body>','<a href="$4">$2</a><br/>','<table>'" now > $OUTPUT_HTML
 
-$MX "seq=$CONTENT " "pft=@avaliacao/pft/html.pft" now >> $OUTPUT_HTML
+$MX "seq=$CONTENT " lw=9999 "pft=@avaliacao/pft/html.pft" now >> $OUTPUT_HTML
 
 $MX null count=1 "pft='</table></body></html>'" now >> $OUTPUT_HTML
--- www/proc/journalEvaluation/current/linux/getIssuesInfo.bat
+++ www/proc/journalEvaluation/current/linux/getIssuesInfo.bat
@@ -6,6 +6,8 @@ ACRON=$3
 INPUT_FOR_ENDOGENIA_PROC=$4
 INPUT_FOR_HISTORY_PROC=$5
 
+FILE_RESULT_DIFF=temp/je_result_diff.txt
+
 echo Issue $2
 echo c
 ./$PATH_SHELL/WriteLog.bat $AVALLOG $ACRON $ISSN  Authors
@@ -13,6 +15,10 @@ $MX cipar=$CPFILE ARTIGO btell=0 "HR=S$ISSN$" lw=9999 "pft=if p(v83) then (v31[1
 
 echo d
 ./$PATH_SHELL/WriteLog.bat $AVALLOG $ACRON $ISSN  History
-$MX cipar=$CPFILE ARTIGO btell=0 "HR=S$ISSN$" lw=9999 "pft=if size(s(v112,v114))=16 and p(v112) and p(v114) then v112*0.4,'-',v112*4.2,'-',v112*6.2,'|',v114*0.4,'-',v114*4.2,'-',v114*6.2,' 1'/ fi" now >> $INPUT_FOR_HISTORY_PROC.seq
+#$MX cipar=$CPFILE ARTIGO btell=0 "HR=S$ISSN$" lw=9999 "pft=if size(s(v112,v114))=16 and p(v112) and p(v114) then v112*0.4,'-',v112*4.2,'-',v112*6.2,'|',v114*0.4,'-',v114*4.2,'-',v114*6.2,' 1'/ fi" now >> $INPUT_FOR_HISTORY_PROC.seq
+
+$MX cipar=$CPFILE ARTIGO btell=0 "HR=S$ISSN$" lw=9999 "pft=if size(s(v112,v114))=16 and p(v112) and p(v114) then v114,'|',v112/ fi" now > temp/je_date_range_list.txt
+./$PATH_COMMON/linux/calculateDateDiff.bat temp/je_date_range_list.txt $INPUT_FOR_HISTORY_PROC.seq
+
 
 
