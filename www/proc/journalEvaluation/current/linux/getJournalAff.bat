afaded5084b96a204bf13f542ba7a599f002b14c
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-11-10 19:05:28 +0000

    processamento de avaliacao de periodicos de permanencia 200910

--- www/proc/journalEvaluation/current/linux/getJournalAff.bat
+++ www/proc/journalEvaluation/current/linux/getJournalAff.bat
@@ -8,7 +8,7 @@
 PROCESS_DATE=`date +%Y%m%d`
 ACRON=$2
 SEQFILE=$3
-$MX "seq=$SEQFILE " lw=9999 "pft='./$PATH_SHELL/getIssuesAff.bat $1 ',v1,' $ACRON ' /" now >temp/avaliacao_lastIssuesAff.bat
-chmod 775 temp/avaliacao_lastIssuesAff.bat
-./temp/avaliacao_lastIssuesAff.bat
+$MX "seq=$SEQFILE " lw=9999 "pft='./$PATH_CURRENT_SHELLS/getIssuesAff.bat $1 ',v1,' $ACRON ' /" now > temp/je_lastIssuesAff.bat
+chmod 775 temp/je_lastIssuesAff.bat
+./temp/je_lastIssuesAff.bat
 
--- www/proc/journalEvaluation/current/linux/improveAff.bat
+++ www/proc/journalEvaluation/current/linux/improveAff.bat
@@ -1,23 +1,23 @@
 . $1
 
+echo `date '+%Y.%m.%d %H:%M:%S'` Executing $0 $1 $2 $3 $4 $5
 
-$MX "seq=$SEQFILEALLISSUESELECTED " lw=9999 "pft='./$PATH_SHELL/getIssuesAff.bat $1 ',v1/" now > temp/je_allissuesselectedaff.bat
+$MX "seq=$FILE_SELECTED_ISSUES.txt " lw=9999 "pft=if size(v1)>0 then './$PATH_CURRENT_SHELLS/getIssuesAff.bat $1 ',v1/ fi" now > temp/je_allissuesselectedaff.bat
 chmod 775 temp/je_allissuesselectedaff.bat
 ./temp/je_allissuesselectedaff.bat
 
-if [ -f temp/je_1 ]
-then
-    rm temp/je_1
-fi
-cat $AFFSEQ | sort -u > temp/je_1
+cp $SEQ_DB_v70 temp/je_1
+cat temp/je_1 | sort -u > $SEQ_DB_v70
 
+echo Create DB_v70 from seq
+$MX "seq=$SEQ_DB_v70" create=$DB_v70 now -all
+$MX $DB_v70 fst=@$PATH_COMMON/fst/v70.fst fullinv=$DB_v70
 
-$MX seq=$AFFSEQ create=temp/v70 now -all
-$MX temp/v70 fst=@$PATH_COMMON/fst/v70.fst fullinv=temp/v70
+echo Gerar um gizmo incompleta (inst-city) para completa
+$MX $DB_v70 btell=0 "completa" lw=99999 "pft=@$PATH_COMMON/pft/aff_completa.pft" now | sort -u > $DB_v70_completa.seq
+$MX seq=$DB_v70_completa.seq create=$DB_v70_completa now -all
+$MX $DB_v70_completa fst=@$PATH_COMMON/fst/v70.fst fullinv=$DB_v70_completa
 
-# gerar um gizmo incompleta (inst-city) para completa
-$MX temp/v70 btell=0 "completa" lw=99999 "pft=@$PATH_COMMON/pft/aff_completa.pft" now | sort -u > temp/aff_completa.seq
-$MX seq=temp/aff_completa.seq create=temp/aff_completa now -all
-$MX temp/aff_completa fst=@$PATH_COMMON/fst/v70.fst fullinv=temp/aff_completa
+$MX $DB_v70 btell=0 "parcial" lw=99999 "proc='d970',ref(['$DB_v70_completa']l(['$DB_v70_completa']v2^*,v2^c,v2^s,v2^p),'a970{',v2,'{')" copy=$DB_v70 now -all
 
-$MX temp/v70 btell=0 "parcial" lw=99999 "proc='d970',ref(['temp/aff_completa']l(['temp/aff_completa']v2^*,v2^c,v2^s,v2^p),'a970{',v2,'{')" copy=temp/v70 now -all
+echo `date '+%Y.%m.%d %H:%M:%S'` Executed $0 $1 $2 $3 $4 $5
