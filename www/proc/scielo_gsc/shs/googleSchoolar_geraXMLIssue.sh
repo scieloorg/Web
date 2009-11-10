38a1f0a015325d768924a683bd74bb6025701abc
fabio.santos <fabio.santos@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-11-10 16:51:55 +0000

    ajustes em processamento do Google Schoolar
    git-svn-id: http://svn.reddes.bvsalud.org/scielo-metodologia/trunk@3853 7b3c32f5-e272-4d6f-9936-548b95b19007

--- www/proc/scielo_gsc/shs/googleSchoolar_geraXMLIssue.sh
+++ www/proc/scielo_gsc/shs/googleSchoolar_geraXMLIssue.sh
@@ -14,7 +14,7 @@ echo "<articles>" >> output/googleSchoolar/$1.xml
 
 echo "Y.*="$database_title".*">databases/google.cip
 
-cisis/mx cipar=databases/google.cip $database_article "proc=@proc/Article.prc" "proc=('Ggizmo/googleSchoolar')" btell=0  "IV=$1$ and (tp=o or tp=h or tp=c or tp=r)" "proc='d32001'" lw=99999 pft=@formats/googleSchoolar_XMLPubMed.pft -all now >> output/googleSchoolar/$1.xml
+$cisis_dir/mx cipar=databases/google.cip $database_article "proc=@proc/Article.prc" "proc=('Ggizmo/googleSchoolar')" btell=0  "IV=$1$ and (tp=o or tp=h or tp=c or tp=r)" "proc='d32001'" lw=99999 pft=@formats/googleSchoolar_XMLPubMed.pft -all now >> output/googleSchoolar/$1.xml
 
 echo "</articles>" >> output/googleSchoolar/$1.xml
 #rm google.cip
