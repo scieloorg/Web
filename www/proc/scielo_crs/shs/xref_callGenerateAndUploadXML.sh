454e79dad7b977135645fd9a5907f8ce2d0cc57d
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2009-11-11 14:21:59 +0000

    #182 Geração de DOI para artigos provisional
    git-svn-id: http://svn.reddes.bvsalud.org/scielo-metodologia/trunk@3857 7b3c32f5-e272-4d6f-9936-548b95b19007

--- www/proc/scielo_crs/shs/xref_callGenerateAndUploadXML.sh
+++ www/proc/scielo_crs/shs/xref_callGenerateAndUploadXML.sh
@@ -10,9 +10,9 @@ ISSN_OR_PID=$2
 
 if [ "@$ISSN_OR_PID" == "@" ]
 then
-	$cisis_dir/mx cipar=$MYCIPFILE ARTIGO_DB "btell=0" "hr=$" lw=9999 "pft='$conversor_dir/shs/xref_generateAndUploadXML.sh ',if p(v881) then v881, else v880 fi,' DO',ref(['XREF_DOI_REPORT']l(['XREF_DOI_REPORT']'hr=',if p(v881) then v881, else v880 fi),if v30<>'error' then 'NOT' fi),' $PROCESS_ONLY_NEW'/" now > $MYTEMP/x.sh
+	$cisis_dir/mx cipar=$MYCIPFILE ARTIGO_DB "btell=0" "hr=$" lw=9999 "proc='d9980a9980{',if p(v891) then v891 else if p(v881) then v881, else v880 fi fi,'{'" "pft='$conversor_dir/shs/xref_generateAndUploadXML.sh ',v9880 ,' DO',ref(['XREF_DOI_REPORT']l(['XREF_DOI_REPORT']'hr=',v9980),if v30<>'error' then 'NOT' fi),' $PROCESS_ONLY_NEW'/" now > $MYTEMP/x.sh
 else
-	$cisis_dir/mx cipar=$MYCIPFILE ARTIGO_DB "btell=0" "hr=S$ISSN_OR_PID$ or hr=$ISSN_OR_PID$" lw=9999 "pft='$conversor_dir/shs/xref_generateAndUploadXML.sh ',if p(v881) then v881, else v880 fi,' DO',ref(['XREF_DOI_REPORT']l(['XREF_DOI_REPORT']'hr=',if p(v881) then v881, else v880 fi),if v30<>'error' then 'NOT' fi),' $PROCESS_ONLY_NEW'/" now > $MYTEMP/x.sh
+	$cisis_dir/mx cipar=$MYCIPFILE ARTIGO_DB "btell=0" "hr=S$ISSN_OR_PID$ or hr=$ISSN_OR_PID$" lw=9999  "proc='d9980a9980{',if p(v891) then v891 else if p(v881) then v881, else v880 fi fi,'{'" "pft='$conversor_dir/shs/xref_generateAndUploadXML.sh ',v9980,' DO',ref(['XREF_DOI_REPORT']l(['XREF_DOI_REPORT']'hr=',v9880),if v30<>'error' then 'NOT' fi),' $PROCESS_ONLY_NEW'/" now > $MYTEMP/x.sh
 fi
 
 chmod 775 $MYTEMP/x.sh
