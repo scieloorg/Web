44e2a5494c24bfbc94d236969fd3c8aec806ca8c
jamil.atta <jamil.atta@7b3c32f5-e272-4d6f-9936-548b95b19007>
2010-04-20 16:51:48 +0000

    Space Tab

--- www/htdocs/index.php
+++ www/htdocs/index.php
@@ -1,9 +1,7 @@
 <?php
+  $redirect = "./scielo.php";
+  if ($_REQUEST['lang'])
+          $redirect .= "?lang=" . $_REQUEST['lang'];
 
-
-	$redirect = "./scielo.php";
-	if ($_REQUEST['lang'])
-		$redirect .= "?lang=" . $_REQUEST['lang']; 
-	
-	header("Location: " . $redirect);
+  header("Location: " . $redirect);
 ?>
