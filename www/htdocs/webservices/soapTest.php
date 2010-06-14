84e8c91de73a1250729b7116df2c2965151058cf
jamil.atta <jamil.atta@7b3c32f5-e272-4d6f-9936-548b95b19007>
2010-06-14 19:58:43 +0000

    WebService with php5
    git-svn-id: http://svn.reddes.bvsalud.org/scielo-metodologia/trunk@4092 7b3c32f5-e272-4d6f-9936-548b95b19007

---
+++ www/htdocs/webservices/soapTest.php
@@ -0,0 +1,69 @@
+<?php
+require_once('nusoap/nusoap.php');
+
+if(!isset($_REQUEST['service'])){
+	die("missing parameter <i>service</i>");
+}
+
+
+$service = $_REQUEST['service'];
+
+$clientesoap = new soapclient('http://'.$_SERVER["HTTP_HOST"].'/webservices/indexBVS.php');
+
+switch($service){
+	case "search":
+		if(!isset($_REQUEST['expression'])){
+			die("missing parameter <i>expression</i>");
+		}
+		if(!isset($_REQUEST['from'])){
+			die("missing parameter <i>from</i>");
+		}
+		if(!isset($_REQUEST['count'])){
+			die("missing parameter <i>count</i>");
+		}
+		$param = array('expression' => $_REQUEST['expression'],'from' => $_REQUEST['from'],'count' => $_REQUEST['count']);
+		$resultado = $clientesoap->call('search',$param);
+		break;
+	case "advancedSearch":
+		if(!isset($_REQUEST['index'])){
+			die("missing parameter <i>index</i>");
+		}
+		if(!isset($_REQUEST['expression'])){
+			die("missing parameter <i>expression</i>");
+		}
+		if(!isset($_REQUEST['from'])){
+			die("missing parameter <i>from</i>");
+		}
+		if(!isset($_REQUEST['count'])){
+			die("missing parameter <i>count</i>");
+		}
+		$param = array('index' => $_REQUEST['index'],'expression' => $_REQUEST['expression'],'from' => $_REQUEST['from'],'count' => $_REQUEST['count']);
+		$resultado = $clientesoap->call('advancedSearch',$param);
+		break;
+	case "listRecords":
+		if(!isset($_REQUEST['count'])){
+			die("missing parameter <i>count</i>");
+		}
+		$param = array('count' => $_REQUEST['count']);
+		$resultado = $clientesoap->call('listRecords',$param); // Alterar o nome do serviço. 
+		break;
+	case "lastRecords":		
+		if(!isset($_REQUEST['count'])){
+			die("missing parameter <i>count</i>");
+		}
+		$param = array('count' => $_REQUEST['count']);
+		$clientesoap->response_timeout = 60;
+		$resultado = $clientesoap->call('lastRecords',$param);
+		break;
+}
+
+
+if($_REQUEST['return'] == 'soap'){
+	echo '<h2>Request</h2><pre>' . htmlspecialchars($clientesoap->request, ENT_QUOTES) . '</pre>';
+	echo '<h2>Response</h2><pre>' . htmlspecialchars($clientesoap->response, ENT_QUOTES) . '</pre>';
+	echo '<h2>Debug</h2><pre>' . htmlspecialchars($clientesoap->debug_str, ENT_QUOTES) . '</pre>';
+}else{
+	header('Content-type: application/xml; charset="ISO-8859-1"',true);
+	echo $resultado;
+}
+?>
