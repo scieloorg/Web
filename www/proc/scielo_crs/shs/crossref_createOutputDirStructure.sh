34c284ee50362ca48f5e635f1196641c25563fd3
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2010-06-22 13:55:45 +0000

    #217 scielo_crs: se documento já tem doi, verificar se o registra ou não

--- www/proc/scielo_crs/shs/crossref_createOutputDirStructure.sh
+++ www/proc/scielo_crs/shs/crossref_createOutputDirStructure.sh
@@ -13,7 +13,7 @@ echo "#temp file"           >  $conversor_dir/shs/crossref_processSequence.sh
 echo ". crossref_config.sh" >> $conversor_dir/shs/crossref_processSequence.sh
 echo "logDate="$DATE >> $conversor_dir/shs/crossref_processSequence.sh
 
-$cisis_dir/mx $database_dir/artigo/artigo "proc=@$conversor_dir/prc/Article.prc" "btell=0" "tp=h" "lw=999999" "pft=@$conversor_dir/formats/crossref_generateProcessSequence.pft" now >> $conversor_dir/shs/crossref_processSequence.sh
+$cisis_dir/mx $database_dir/artigo/artigo "proc=@$conversor_dir/prc/Article.prc" "btell=0" "tp=h" "lw=999999" "proc='a9191{$depositor_prefix{'" "pft=@$conversor_dir/formats/crossref_generateProcessSequence.pft" now >> $conversor_dir/shs/crossref_processSequence.sh
 echo "--- Done!"
 echo "--- Running crossref_processSequence.sh to Generating toDoList.txt"
 if [ -f toDoList.txt ]
