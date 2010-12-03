0878a869638bc3346470f8130bd95ff07a51d586
SciELO ORG <fabio.batalha@scielo.org>
2010-12-03 18:46:13 -0200

    Branch SciELO Inovacao

--- www/htdocs/index.php
+++ www/htdocs/index.php
@@ -1,7 +1,8 @@
 <?php
-  $redirect = "./scielo.php";
-  if ($_REQUEST['lang'])
-          $redirect .= "?lang=" . $_REQUEST['lang'];
 
-  header("Location: " . $redirect);
+        $redirect = "./scielo.php/scielo.php?script=sci_serial&pid=1808-2394&lng=en&nrm=iso";
+        if ($_REQUEST['lang'])
+                $redirect .= "?lang=" . $_REQUEST['lang'];
+
+        header("Location: " . $redirect);
 ?>
