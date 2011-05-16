98a5fe93e569bd1354e1cb708549af2c9b9db593
robertatakenaka <roberta.takenaka@scielo.org>
2011-05-16 17:10:54 -0300

    proc_doi: include gizmo entities for generation of XML

--- proc/proc_doi/query/shs/generate_xml_node_query.sh
+++ proc/proc_doi/query/shs/generate_xml_node_query.sh
@@ -3,6 +3,6 @@ PID=$2
 
 . $CONFIG
 
-$MX cipar=$CIPFILE ARTIGO btell=0 "hr=$PID or r=$PID" lw=9999 "pft=@pft/xml.pft" now
+$MX cipar=$CIPFILE ARTIGO btell=0 "hr=$PID or r=$PID" gizmo=shs/ent lw=9999 "pft=@pft/xml.pft" now
 
 
