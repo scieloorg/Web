813a56eca68715d67be288e2983fd3bfc151f5fa
robertatakenaka <roberta.takenaka@scielo.org>
2011-07-13 15:09:17 -0300

    1) proc_doi: fixed selection of pid and renaming some files for better understanding 2) subject area: enhancement, subject.iso generates gizmos of subject which are used to generate the  serarea<LANG> inverted files.

--- proc/batch/GeraNewcodeAux.bat
+++ proc/batch/GeraNewcodeAux.bat
@@ -6,27 +6,18 @@ rem Parametro 2: path diret?io tempor?io
 call batch/VerifPresencaParametro.bat $0 @$1 path producao Scielo
 call batch/VerifPresencaParametro.bat $0 @$2 diretorio temporario
 
-call batch/InformaLog.bat $0 x Gera Newcode Auxiliar
+call batch/InformaLog.bat $0 x GeraNewcodeAux Gera Serarea para cada idioma
 
-rem call batch/VerifExisteBase.bat $1/code/newcode
-rem call batch/GeraInvertido.bat $1/code/newcode fst/newcode.fst $1/code/newcode
+call batch/VerifExisteBase.bat $1/code/newcode
+call batch/GeraInvertido.bat $1/code/newcode fst/newcode.fst $1/code/newcode
 
-if [ ! -f $1/code/code.mst ]
+if [ -f gizmo/subject.iso ]
 then
-    if [ -f gizmo/code.iso ]
-    then
-        call batch/Iso2Master.bat gizmo/code.iso $1/code/code
-    fi
+    call batch/Iso2Master.bat gizmo/subject.iso $2/subject
 fi
-
-call batch/VerifExisteBase.bat $1/code/code
-call batch/GeraInvertido.bat $1/code/code fst/newcode.fst $1/code/code
+call batch/VerifExisteBase.bat $2/subject
 
 
-$CISIS_DIR/mx $1/code/code btell=0 "pt-study area" pft=@pft/sa.pft now > $2/sa_pt.seq
-$CISIS_DIR/mx seq=$2/sa_pt.seq create=$2/sa_pt -all now
-$CISIS_DIR/mx $2/sa_pt fst=@fst/sa.fst fullinv/ansi=$2/sa_pt -all now
+$CISIS_DIR/mx $2/subject lw=999 "pft='call batch/GeraSerareaLANG.bat $2/subject ',mfn,' ../bases-work/title/title gizmo/serarea',mpu,v1^l,mpl,' $2'/" now > $2/GeraSerareaLANG.bat
+call $2/GeraSerareaLANG.bat
 
-$CISIS_DIR/mx $1/code/code btell=0 "es-study area" pft=@pft/sa.pft now > $2/sa_es.seq
-$CISIS_DIR/mx seq=$2/sa_es.seq create=$2/sa_es -all now
-$CISIS_DIR/mx $2/sa_es fst=@fst/sa.fst fullinv/ansi=$2/sa_es -all now
