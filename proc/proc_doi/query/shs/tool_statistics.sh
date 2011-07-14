431637c5cc8376b2c6cb095c9dcf599132ffcbc0
robertatakenaka <roberta.takenaka@scielo.org>
2011-07-14 14:56:29 -0300

    subject.iso to generate gizmos of subject area proc_doi: revision of the scripts which generate statistics report

--- proc/proc_doi/query/shs/tool_statistics.sh
+++ proc/proc_doi/query/shs/tool_statistics.sh
@@ -7,14 +7,14 @@ t=`date '+%Y%m%d %H:%M:%S'`
 
 sh ./reglog.sh $BATCH_LOGFILE "$QUERYDB"
 #$MX $QUERYDB +control now>> $BATCH_LOGFILE
-$MXTB $QUERYDB create=$BATCH_TEMP_PATH/numbers "100:'total_doi'/,(if size(v880^*)=23 or size(v880^*)=28 then 'pid_done' fi/),if p(v237) then 'doi'/ fi" class=1000000
+$MXTB $QUERYDB create=$BATCH_TEMP_PATH/numbers "100:'total_doi'/,if p(v237) then 'doi'/ fi,(if size(v880^*)=23 then 'pid_done'/,'pid_doc'/ fi,if size(v880^*)=28 then 'pid_done'/,'pid_ref'/ fi)" class=1000000
 $MX $BATCH_TEMP_PATH/numbers "pft='|',v1,'|',v999,'|'/" now >> $BATCH_LOGFILE
 
 $MX $BATCH_TEMP_PATH/numbers lw=999  "pft=@pft/stat01.pft" now > $BATCH_TEMP_PATH/numbers.prc
 
 sh ./reglog.sh $BATCH_LOGFILE "$QUERYLOGDB"
 #$MX $QUERYLOGDB +control now>> $BATCH_LOGFILE
-$MXTB $QUERYLOGDB create=$BATCH_TEMP_PATH/numbers "100:'total_log'/,(if size(v880)=23 or size(v880)=28 then 'pid_toDo' fi/),if p(v100) then |status=|v100/ fi" class=1000000
+$MXTB $QUERYLOGDB create=$BATCH_TEMP_PATH/numbers "100:'total_log'/,(if size(v880)=23 or size(v880)=28 then 'pid_toDo' fi/),if p(v100) then v100/ fi" class=1000000
 $MX $BATCH_TEMP_PATH/numbers "pft='|',v1,'|',v999,'|'/" now >> $BATCH_LOGFILE
 $MX $BATCH_TEMP_PATH/numbers lw=999 "pft=@pft/stat02.pft" now >> $BATCH_TEMP_PATH/numbers.prc
 
