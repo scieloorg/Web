f1d47ad8f6a3ce3b8363dbc424f493fa5b9d807b
robertatakenaka <roberta.takenaka@scielo.org>
2011-10-06 23:11:45 +0800

    GeraNewcodeAux: generate standard subject list proc_doi: fix a message, fix (v880 => v880^*), fix entities

--- proc/proc_doi/query/call_query.sh
+++ proc/proc_doi/query/call_query.sh
@@ -3,26 +3,15 @@ STATUS=$1
 
 if [ "@$STATUS" == "@" ]
 then
-    echo Missing STATUS new or query
-
+    echo Missing STATUS new or notfound
 else
     dos2unix *.sh
     dos2unix */*.sh
-
-
     . shs/readconfig.sh
-
-
-
-
-
     if [ ! -d $RESULT_PATH ]
     then
         mkdir -p $RESULT_PATH
     fi
-
     RES=$RESULT_PATH/`date '+%Y%m%d%H%M'`.res
-
     sh ./shs/main_do_query.sh config.sh $RES $STATUS
-
 fi
