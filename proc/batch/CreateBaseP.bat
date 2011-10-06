c38aedc85cbb6fc5c478989571743c7fa52482c2
robertatakenaka <roberta.takenaka@scielo.org>
2011-10-06 16:26:30 -0300

    Keeping both GeraPadrao. Now GeraIssue.bat contains the calling to both GeraIssueHasP.bat and GeraIssueNoP.bat. But only one of them must be enabled to run.

--- proc/batch/CreateBaseP.bat
+++ proc/batch/CreateBaseP.bat
@@ -7,14 +7,18 @@ if [ ! -d $NEWDBPATH ]
 then
     mkdir -p $NEWDBPATH
 fi
+if [ -d $NEWDBPATH ]
+then
+    $MX $DB btell=0 "bool=RP$ID" count=1 "pft=mfn,'|',v701/" now > i.txt
+    $MX $DB btell=0 "bool=RP$ID" "pft=mfn,'|',v701/" now | sort -r >> i.txt
 
-$MX $DB btell=0 "bool=RP$ID" count=1 "pft=mfn,'|',v701/" now > i.txt
-$MX $DB btell=0 "bool=RP$ID" "pft=mfn,'|',v701/" now | sort -r >> i.txt
-$MX seq=i.txt count=2 create=$NEWDBPATH/$NEWDB now -all
+    $MX seq=i.txt count=2 create=$NEWDBPATH/$NEWDB now -all
+    $MX $DB btell=0 "bool=P$ID" append=$NEWDBPATH/$NEWDB now -all
 
-$MX $DB btell=0 "bool=P$ID" append=$NEWDBPATH/$NEWDB now -all
-$MX $DB btell=0 "bool=P$ID" "proc='d*'" copy=$DB now -all
-
-echo $NEWDBPATH/$NEWDB >> log/NUMEROS
-$MX $NEWDBPATH/$NEWDB +control now >> log/NUMEROS
-
+    if [ -f $NEWDBPATH/$NEWDB.mst ]
+    then
+        $MX $DB btell=0 "bool=P$ID" "proc='d*'" copy=$DB now -all
+        echo $NEWDBPATH/$NEWDB >> log/NUMEROS
+        $CISIS_DIR/mx $NEWDBPATH/$NEWDB +control now >> log/NUMEROS
+    fi
+fi
