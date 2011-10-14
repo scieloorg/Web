3cb01408924e0c1ed8e644e078778fbaf3b58770
robertatakenaka <roberta.takenaka@scielo.org>
2011-10-14 10:27:59 -0300

    Default must be artigo with p records

--- proc/batch/GeraIssue.bat
+++ proc/batch/GeraIssue.bat
@@ -5,5 +5,5 @@ rem Parametro 2: sigla da revista
 rem Parametro 3: volume-numero do issue
 rem Parametro 4: acao - se for "del" eh para deletar, se ausente eh caso normal
 
-#call batch/GeraIssueHasP.bat $1 $2 $3 $4
-call batch/GeraIssueNoP.bat $1 $2 $3 $4
+call batch/GeraIssueHasP.bat $1 $2 $3 $4
+#call batch/GeraIssueNoP.bat $1 $2 $3 $4
