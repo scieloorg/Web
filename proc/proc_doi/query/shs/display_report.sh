ca1c4581c2ba59400899ec12848f516ee0ad4453
robertatakenaka <roberta.takenaka@scielo.org>
2011-10-26 14:30:58 -0200

    sci_serial: enhancement: present links to the other SciELO collections in which the journal is registered.

--- proc/proc_doi/query/shs/display_report.sh
+++ proc/proc_doi/query/shs/display_report.sh
@@ -4,8 +4,8 @@ QLOG=$3
 QDB=$4
 REPORTFILENAME=$5
 
-$MXTB $QLOG create=l "100:if size(v880^*)=23 then v691,'|art|',v100/ else if size(v880^*)=28 then v691,'|ref|',v100/ fi fi" class=100000
-$MXTB $QDB create=q "100:(if size(v880^*)=23 then v880^c,'|art|ok'/ else if size(v880^*)=28 then v880^c,'|ref|ok'/ fi  fi),if p(v237) then 'doi'/ fi" class=1000000
+$MXTB $QLOG create=l "100:if size(v880^*)=23 then v691,'|art|',v100/,'TOT|art|',v100/,'TOT|   |',v100/ else if size(v880^*)=28 then v691,'|ref|',v100/,'TOT|ref|',v100/,,'TOT|   |',v100/ fi fi" class=100000
+$MXTB $QDB create=q "100:(if size(v880^*)=23 then v880^c,'|art|ok'/,'TOT|art|ok'/,'TOT|   |ok'/ else if size(v880^*)=28 then v880^c,'|ref|ok'/,'TOT|ref|ok'/,,'TOT|   |ok'/ fi  fi),if p(v237) then 'DOI'/ fi" class=1000000
 
 $MX l lw=9999 "pft=v1,'|',v999/" now > $REPORTFILENAME.tmp
 $MX q lw=9999 "pft=v1,'|',v999/" now >> $REPORTFILENAME.tmp
