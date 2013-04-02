c20023b9acf0e008675bda9f1729aae0e4e2eb36
fabiobatalha <fabiobatalha@gmail.com>
2013-04-02 17:22:16 -0300

    Incluindo ajustes

--- htdocs/pressrelease/journalmanager.php
+++ htdocs/pressrelease/journalmanager.php
@@ -1,10 +1,49 @@
 <?php
 $journal_manager_api = "http://localhost/api/v1/";
+$mencached_host = "localhost:11211";
+
+function citation_display($data){
+
+    $authors  = array();
+
+    foreach ($data['meta']['article']['authors']->AUTHOR as $author){
+        $name = array();
+        
+        if ($author->NAME){
+           array_push($name, $author->NAME);
+        }
+        
+        if ($author->SURNAME){
+            array_push($name, $author->SURNAME);
+        }
+
+        array_push($authors, implode(', ', $name));
+    }
+
+    $meta_arr = array(implode('; ', $authors),
+                      $data['meta']['article']['title'],
+                      $data['meta']['issue']['journal_abbrev_title'],
+                      $data['meta']['issue']['city'],
+                      $data['meta']['issue']['year'],
+                      implode(' ', array($data['meta']['issue']['vol'], $data['meta']['issue']['num'])),
+                      implode('-', array($data['meta']['article']['fpage'], $data['meta']['article']['lpage'])),
+                      );
+
+    $citation = implode( ', ', $meta_arr).'.';
+    
+    for ($x=1; $x<=count($citation); $x++){
+        if ($citation[$x].str_replace(' ','').str_replace('-','') === '') {
+            unset($citation[$x]);
+        }
+    }
+
+    return $citation;
+}
 
 function load_article_meta($url){
 
     $xml_meta = file_get_contents($url."&debug=xml");
-    
+
     $article = new SimpleXMLElement($xml_meta);
 
     $article_meta = array();
@@ -21,8 +60,29 @@ function load_article_meta($url){
     $article_meta['article'] = array();
     $article_meta['article']['title'] = $issue->ARTICLE->TITLE;
     $article_meta['article']['authors'] = $issue->ARTICLE->AUTHORS->AUTH_PERS;
+    $article_meta['article']['fpage'] = $issue->ARTICLE->attributes()->FPAGE;
+    $article_meta['article']['lpage'] = $issue->ARTICLE->attributes()->LPAGE;
 
     return $article_meta;
+}
+
+function load_issue_meta($url){
+
+    $xml_meta = file_get_contents($url."&debug=xml");
+
+    $issue = new SimpleXMLElement($xml_meta);
+
+    $meta = array();
+
+    $meta['issue'] = array();
+    $meta['issue']['journal_abbrev_title'] = $issue->TITLEGROUP->SHORTTITLE;
+    $meta['issue']['vol'] = $issue->ISSUE->STRIP->VOL;
+    $meta['issue']['num'] = $issue->ISSUE->STRIP->NUM;
+    $meta['issue']['city'] = $issue->ISSUE->STRIP->CITY;
+    $meta['issue']['month'] = $issue->ISSUE->STRIP->MONTH;
+    $meta['issue']['year'] = $issue->ISSUE->STRIP->YEAR;
+
+    return $meta;
 }
 
 function get_articles_press_releases_by_journal_id($id){
@@ -83,7 +143,14 @@ function get_press_release($id){
     $result = array();
 
     $result['prs'] = $json;
-    $result['meta'] = load_article_meta($json['url']);
+
+    if ($jsonp['type'] === 'article'){
+        $result['meta'] = load_article_meta($json['url']);
+    }else{
+        $result['meta'] = load_issue_meta($json['url']);
+    }
+
+    $result['citation'] = citation_display($result);
 
     return $result;
 }
--- htdocs/pressrelease/pressrelease_articles_list.php
+++ htdocs/pressrelease/pressrelease_articles_list.php
@@ -6,7 +6,7 @@ $articles_prs = get_articles_press_releases_by_journal_id($_REQUEST['id']);
 echo('<ul class="pressReleases">');
 foreach ($articles_prs as $article){
     echo('<li>');
-    echo('<a href="/pressrelease/pressrelease.php?id='.$article['id'].'"><b>'.substr($article["created_at"], 0, 7).' '.$article["issue_label"].'</b><br/>'.substr($article["title"], 0, 10).'</a>');
+    echo('<a href="/pressrelease/pressrelease_display.php?id='.$article['id'].'"><b>'.substr($article["created_at"], 0, 7).' '.$article["issue_label"].'</b><br/>'.substr($article["title"], 0, 10).'</a>');
     echo('</li>');
 }
 echo('</ul>');
