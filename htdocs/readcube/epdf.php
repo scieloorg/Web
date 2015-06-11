68cefbdad914fc3c43210fd875f6c9da6fe7dfa0
fabiobatalha <fabiobatalha@gmail.com>
2015-06-11 14:26:56 -0300

    Ajustes para incluir link de epdf nas demais verões do documento

--- htdocs/readcube/epdf.php
+++ htdocs/readcube/epdf.php
@@ -2,19 +2,19 @@
 $doi = $_REQUEST['doi'];
 $pid = $_REQUEST['pid'];
 $pdf_path = $_REQUEST['pdf_path'];
+$lang = $_REQUEST['lang'];
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
-    <!-- #1 Automatic Redirection to the traditional PDF for browsers with JavaScript disabled -->
     <noscript>
         <META HTTP-EQUIV="Refresh" CONTENT="0;URL=/pdf/<?=$pdf_path?>">
     </noscript>
-    <!-- #2 Meta Tag containing DOI -->
     <meta name="DOI" content="<?=$doi?>">
-    <!-- #3 Meta Tag containing PDF URL used for client side redirection -->
     <meta name="PDF_URL" content="/pdf/<?=$pdf_path?>">
-    <!-- #4 ReadCube Embedded PDF Script -->
+    <?if ($lang != '') {?>
+        <meta name="LANG" content="<?=$lang?>">
+    <?}?>
 </head>    
 <body/>
 <script src="http://content.readcube.com/scielo/epdf.js" type="text/javascript"></script>
