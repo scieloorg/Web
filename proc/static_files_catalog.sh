49d4782b554809307e9a4504cd793f01527a1ed2
Fabio Batalha <fabiobatalha@gmail.com>
2016-02-02 11:17:53 -0200

    Incluindo geração de listagem de seções.

--- proc/static_files_catalog.sh
+++ proc/static_files_catalog.sh
@@ -1,7 +1,7 @@
 BASEDIR=$(dirname $0)
 echo $BASEDIR
-cd $BASEDIR/../bases
-
+cd $BASEDIR
+cd ../bases
 if [ -d "pdf" ]; then
     cd pdf
     find . -name "*.pdf" > ../../htdocs/static_pdf_files.txt
@@ -19,3 +19,6 @@ if [ -d "xml" ]; then
     find . -name "*.xml" > ../../htdocs/static_xml_files.txt
     cd ..
 fi
+
+cd ../proc
+cisis/mx ../bases/issue/issue "pft=if p(v49) then (v35[1],v65[1]*0.4,s(f(val(s(v36[1]*4.3))+10000,2,0))*1.4,'|',v49^l,'|',v49^c,'|',v49^t,/) fi" -all now > ../htdocs/static_section_catalog.txt
