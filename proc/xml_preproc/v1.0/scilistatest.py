df59aac616df25df996ef0d32b8c62d77e143a08
robertatakenaka <roberta.takenaka@scielo.org>
2018-01-19 17:55:51 -0200

    tk642
    Fixes #642

--- proc/xml_preproc/v1.0/scilistatest.py
+++ proc/xml_preproc/v1.0/scilistatest.py
@@ -23,30 +23,20 @@ print '----------------------------------------------\n'
 #Builds a list from scilistxml.lst
 lst = []
 f = open('../serial/scilistaxml.lst', 'r')
+
 for row in f:
     lst.append(row.split())
 f.close()
 
 #Test issues in scilistaxml.lst
-n = 0
 for issue in lst:
-    n += 1
-    linha = ' '.join(issue)
-    print linha
-    if len(issue) == 0:
-        print('Error: Linha {}: remover linha vazia\n'.format(n))
-        fbug.write('Error: Linha {}: remover linha vazia\n'.format(n))
-        continue
-    elif len(issue) not in [2, 3]:
-        print('Error: Linha {}: corrigir {} \n'.format(n, linha))
-        fbug.write('Error: Linha {}: corrigir {} \n'.format(n, linha))
-        continue
-    print('Executar')
+    print 'Checking line> ' + str(issue)
     try:
         acron = issue[0]
         acron = issue[1]
+        print 'Issue OK'
         if os.path.exists('/bases/xml.000/serial/' + issue[0] + '/' + issue[1]):
-            print 'Issue OK\n'
+            print 'Copying...\n'
         else:
             print      '%s %s \t%s\n' % (issue[0],issue[1],'fasciculo nao localizado - verificar')
             fbug.write('%s %s \t%s\n' % (issue[0],issue[1],'fasciculo nao localizado - verificar'))
