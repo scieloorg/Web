1f9a9381e2c83159df1fc1b0c3d1c3d21e1be17e
robertatakenaka <roberta.takenaka@scielo.org>
2018-01-19 17:55:51 -0200

    tk642
    Fixes #642

--- proc/xml_preproc/coletaxml.sh
+++ proc/xml_preproc/coletaxml.sh
@@ -1,27 +1,2 @@
-#!/bin/bash
-. xml_preproc.ini
-
-echo Processamento $XMLPREPROC_COLLECTION_NAME
-echo Ordenando scilistas
-#remove duplicidades das scilistas
-#scilistaxml.lst
-sort -u ../serial/scilistaxml.lst > ../serial/scilistaxml_ord
-cat ../serial/scilistaxml_ord > ../serial/scilistaxml.lst
-
-#scilista.lst
-sort -u ../serial/scilista.lst > ../serial/scilista_ord
-cat ../serial/scilista_ord > ../serial/scilista.lst
-
-echo Executando getbasesxml4proc.py para coleta das Bases XML
-echo
-
-./getbasesxml4proc.py
-
-echo Fazendo a juncao das listas 'scilistaxml.lst' e 'scilista.lst'
-echo
-./joinlist.py
-
-echo ------------------------
-echo Proximo passo:
-echo
-echo Executar processar.sh
+# Substituido por scilistatest_and_coletaxml.py
+# que faz tanto scilistatest + coletaxml
