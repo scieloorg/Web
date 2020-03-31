15c0bf8517593ab22ce03f46f43464e69cb20e39
Roberta Takenaka <takenaka.roberta@gmail.com>
2020-03-31 16:22:19 -0300

    Ajusta para chamar PreGeraScielo.bat

--- proc/PreGeraPadrao.bat
+++ proc/PreGeraPadrao.bat
@@ -1,14 +1,14 @@
 export PATH=$PATH:.
 export TABS=tabs
 rem Este arquivo ?uma chamada para o 
-rem GeraScielo.bat com par?etros STANDARD
+rem PreGeraScielo.bat com par?etros STANDARD
 
 clear
 echo === ATENCAO ===
 echo 
 echo Este arquivo executara o seguinte comando
-echo GeraScielo.bat .. /scielo/web log/GeraPadrao.log adiciona
+echo PreGeraScielo.bat .. /scielo/web log/PreGeraPadrao.log adiciona
 echo 
 echo Tecle CONTROL-C para sair ou ENTER para continuar...
 
-GeraScielo.bat .. .. log/GeraPadrao.log adiciona
+PreGeraScielo.bat .. .. log/PreGeraPadrao.log adiciona
